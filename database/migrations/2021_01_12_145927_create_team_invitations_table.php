ea8d1b5a896ceca2c0834a907ecd615cb3b28080
Ned Zimmerman <ned@bight.dev>
2021-01-12 11:01:13 -0400

    chore(deps): update to Jetstream 2.x

---
+++ database/migrations/2021_01_12_145927_create_team_invitations_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTeamInvitationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('team_invitations', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('team_invitations');
+    }
+}
