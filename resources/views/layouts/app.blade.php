3e78b77c35ab2c051bdd7ac7fdad309eded9a089
Ned Zimmerman <ned@bight.dev>
2021-01-29 12:09:51 -0400

    chore(deps): update Looseleaf

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
         <title>{{ config('app.name', 'Accessibility in Action') }}</title>
 
         <!-- Styles -->
-        <link rel="stylesheet" href="https://unpkg.com/@accessibility-in-action/looseleaf@1.0.0-alpha.3/style.min.css">
+        <link rel="stylesheet" href="https://unpkg.com/@accessibility-in-action/looseleaf@1.0.0-alpha.4/style.min.css">
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
         <!-- Scripts -->
