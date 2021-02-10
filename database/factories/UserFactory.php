63e9f168b9aa075033c0dec1726e0083a43a6873
Ned Zimmerman <ned@bight.dev>
2021-02-10 16:51:00 -0400

    fix: update User factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -22,8 +22,10 @@ class UserFactory extends Factory
      */
     public function definition()
     {
+        $name = $this->faker->name;
         return [
-            'name' => $this->faker->name,
+            'name' => $name,
+            'slug' => Str::slug($name),
             'email' => $this->faker->unique()->safeEmail,
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
