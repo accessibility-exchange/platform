b2cc1222f813bf738690fd627682b923b87b5580
Ned Zimmerman <ned@bight.dev>
2021-02-11 11:48:01 -0400

    fix: adjust users.index layout

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -6,10 +6,15 @@
     <div class="flow">
         @if($users)
             <filter-container class="flow">
-            <div class="field"><label for="region">{{ __('Province or territory') }}</label> <x-region-select /></div>
+            <div class="field">
+                <label for="region">{{ __('Province or territory') }}</label>
+                <x-region-select />
+            </div>
             <p aria-live="polite" data-filter-results="{{ __('person') }}/{{ __('people') }}"></p>
             @foreach($users as $user)
-            <p data-filter-region="{{ $user->region }}"><a href="{{ route('users.show', $user) }}">{{ $user->name }}</a></p>
+            <div data-filter-region="{{ $user->region }}">
+                <a href="{{ route('users.show', $user) }}">{{ $user->name }}</a>
+            </div>
             @endforeach
             </filter-container>
         @else
