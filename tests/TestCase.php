a772afaaca09a92fe973c8d66c938c618f8d917e
Ned Zimmerman <ned@bight.dev>
2021-02-17 15:42:50 -0400

    test: adjust test configuration with fully namespaced class

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,6 +3,7 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Mcamara\LaravelLocalization\LaravelLocalization;
 
 abstract class TestCase extends BaseTestCase
 {
