2b99e88f7a21ead3bbbf94179f2f641b883b0864
Ned Zimmerman <ned@bight.dev>
2021-02-17 13:28:48 -0400

    fix: revert tests for the moment

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,17 +7,4 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
-
-    protected function refreshApplicationWithLocale($locale)
-    {
-        self::tearDown();
-        putenv(LaravelLocalization::ENV_ROUTE_KEY . '=' . $locale);
-        self::setUp();
-    }
-
-    protected function tearDown()
-    {
-        putenv(LaravelLocalization::ENV_ROUTE_KEY);
-        parent::tearDown();
-    }
 }
