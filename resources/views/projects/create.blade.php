fedb494496329cee3121d1eee538baeaa44635c4
Ned Zimmerman <ned@bight.dev>
2021-08-10 21:18:20 -0300

    fix: update components in project views

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -13,17 +13,17 @@
 
     <form id="create-project" action="{{ localized_route('projects.store', $entity) }}" method="POST" novalidate>
         @csrf
-        <x-input id="entity_id" type="hidden" name="entity_id" :value="$entity->id" required />
+        <x-hearth-input id="entity_id" type="hidden" name="entity_id" :value="$entity->id" required />
 
         <div class="field">
-            <x-label for="name" :value="__('project.label_name')" />
-            <x-input id="name" type="text" name="name" required />
+            <x-hearth-label for="name" :value="__('project.label_name')" />
+            <x-hearth-input id="name" type="text" name="name" required />
         </div>
 
         <x-date-input :label="__('project.label_start_date')" name="start_date" :value="old('start_date', '')" />
 
         <x-date-input :label="__('project.label_end_date')" name="end_date" :value="old('end_date', '')" />
 
-        <x-button>{{ __('project.action_create') }}</x-button>
+        <x-hearth-button>{{ __('project.action_create') }}</x-hearth-button>
     </form>
 </x-app-layout>
--- resources/views/projects/edit.blade.php
+++ resources/views/projects/edit.blade.php
@@ -6,21 +6,21 @@
     </x-slot>
 
     <!-- Form Validation Errors -->
-    @include('partials.validation-errors') 
+    @include('partials.validation-errors')
 
     <form id="update-project" action="{{ localized_route('projects.update', $project) }}" method="POST" novalidate>
         @csrf
-        @method('PUT') 
+        @method('PUT')
         <div class="field">
-            <x-label for="name" :value="__('project.label_name')" />
-            <x-input id="name" type="text" name="name" :value="old('name', $project->name)" required />
+            <x-hearth-label for="name" :value="__('project.label_name')" />
+            <x-hearth-input id="name" type="text" name="name" :value="old('name', $project->name)" required />
         </div>
 
         <x-date-input :label="__('project.label_start_date')" name="start_date" :value="old('start_date', $project->start_date->format('Y-m-d'))" />
 
         <x-date-input :label="__('project.label_end_date')" name="end_date" :value="old('end_date', $project->end_date ? $project->end_date->format('Y-m-d') : '')" />
 
-        <x-button>{{ __('forms.save_changes') }}</x-button>
+        <x-hearth-button>{{ __('forms.save_changes') }}</x-hearth-button>
     </form>
 
     <h2>
@@ -31,18 +31,18 @@
 
     <form action="{{ localized_route('projects.destroy', $project) }}" method="POST" novalidate>
         @csrf
-        @method('DELETE') 
+        @method('DELETE')
 
         <div class="field">
-            <x-label for="current_password" :value="__('auth.label_current_password')" />
-            <x-input id="current_password" type="password" name="current_password" required />
+            <x-hearth-label for="current_password" :value="__('auth.label_current_password')" />
+            <x-hearth-input id="current_password" type="password" name="current_password" required />
             @error('current_password', 'destroyProject')
                 <x-validation-error>{{ $message }}</x-validation-error>
             @enderror
         </div>
 
-        <x-button>
+        <x-hearth-button>
             {{ __('project.action_delete') }}
-        </x-button>
+        </x-hearth-button>
     </form>
 </x-app-layout>
