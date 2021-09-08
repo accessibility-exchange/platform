dab1a8bf0d7ecc6b22b6d85b53c491564102f4f9
Ned Zimmerman <ned@bight.dev>
2021-09-08 17:46:21 -0300

    feat: add lazy eager loading to entity show

--- app/Http/Controllers/EntityController.php
+++ app/Http/Controllers/EntityController.php
@@ -62,7 +62,9 @@ class EntityController extends Controller
      */
     public function show(Entity $entity)
     {
-        return view('entities.show', ['entity' => $entity]);
+        $entity->load('pastProjects', 'currentProjects');
+
+        return view('entities.show', compact('entity'));
     }
 
     /**
