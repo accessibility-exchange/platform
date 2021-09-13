9fdb607cd0b03f028bdea7adb61305d9a5ba3730
Ned Zimmerman <ned@bight.dev>
2021-09-13 12:37:51 -0300

    chore(l10n): translate resource.php into Portuguese, Brazilian

---
+++ resources/lang/pt/resource.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'create_title' => 'Create a resource',
+    'label_title' => 'Resource title',
+    'label_language' => 'Resource language',
+    'label_summary' => 'Resource summary',
+    'action_create' => 'Create resource',
+    'index_title' => 'Resources',
+    'plural' => 'resources',
+    'singular' => 'resource',
+    'edit_title' => 'Edit resource “:title”',
+    'edit_resource' => 'Edit resource',
+    'action_delete' => 'Delete resource',
+    'delete_title' => 'Delete resource',
+    'delete_intro' => 'Your resource will be deleted and cannot be recovered. If you still want to delete your resource, please enter your current password to proceed.',
+    'create_succeeded' => 'Your resource has been created.',
+    'update_succeeded' => 'Your resource has been updated.',
+    'destroy_succeeded' => 'Your resource has been deleted.',
+    'none_found' => 'No resources found.',
+];
