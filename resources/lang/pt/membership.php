9e74f1637aee2f2708a8dc6a5069d2cf9a1eed96
Ned Zimmerman <ned@bight.dev>
2021-09-13 12:39:14 -0300

    chore(l10n): translate membership.php into Portuguese, Brazilian

---
+++ resources/lang/pt/membership.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'role_update_succeeded' => ':user’s role has been updated.',
+    'remove_member_succeeded' => ':user has been removed from :memberable.',
+    'edit_user_role_title' => 'Edit :user’s role',
+    'edit_user_role_intro' => 'Modifying the role will change :user’s privileges for their :memberable membership.',
+];
