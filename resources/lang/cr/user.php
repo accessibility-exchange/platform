772a0e76dc6b2e7a35661992853692194ddcd8e1
Ned Zimmerman <ned@bight.dev>
2021-09-13 12:38:14 -0300

    chore(l10n): translate user.php into Cree

---
+++ resources/lang/cr/user.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'settings' => 'Settings',
+    'account' => 'Account',
+    'label_name' => 'Full name',
+    'label_locale' => 'Preferred language',
+    'delete_account' => 'Delete account',
+    'delete_account_intro' => 'Your account will be deleted and cannot be recovered. If you still want to delete your account, please enter your current password to proceed.',
+    'action_delete_account' => 'Delete my account',
+    'settings_update_succeeded' => 'Your settings have been saved.',
+    'destroy_succeeded' => 'Your account has been deleted.',
+    'profile_title' => 'Your consultant page',
+    'no_consultant_page' => 'You don’t have a consultant page. If you want, you can :link.',
+    'create_consultant_page' => 'create your page',
+    'organizations_title' => 'Your organizations',
+    'no_organization' => 'You aren’t part of any organizations. If you want, you can :create_link.',
+    'create_organization' => 'create an organization',
+    'entities_title' => 'Your regulated entities',
+    'no_entity' => 'You aren’t part of any regulated entities. If you want, you can :create_link.',
+    'create_entity' => 'create a regulated entity',
+];
