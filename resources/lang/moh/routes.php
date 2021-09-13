f2e4de7bd02263d4b0f9152add9fe9ab423ee6a9
Ned Zimmerman <ned@bight.dev>
2021-09-13 12:38:12 -0300

    chore(l10n): translate routes.php into Mohawk

---
+++ resources/lang/moh/routes.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'dashboard' => 'dashboard',
+    'logout' => 'logout',
+    'login' => 'login',
+    'register' => 'register',
+    'verification.verify' => '/verify-email/{id}/{hash}',
+];
