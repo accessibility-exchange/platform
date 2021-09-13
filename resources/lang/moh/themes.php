1867de883523f971768e10bb435982afcc552e3f
Ned Zimmerman <ned@bight.dev>
2021-09-13 12:38:32 -0300

    chore(l10n): translate themes.php into Mohawk

---
+++ resources/lang/moh/themes.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'label_theme' => 'Colour theme',
+    'system' => 'System theme',
+    'light' => 'Light theme',
+    'dark' => 'Dark theme',
+    'black-yellow' => 'Black on yellow theme',
+    'yellow-black' => 'Yellow on black theme',
+    'grey-dark' => 'Grey on dark grey theme',
+    'light-dark' => 'Light grey on dark grey theme',
+    'grey-white' => 'Grey on white theme',
+    'black-brown' => 'Black on brown theme'
+];
