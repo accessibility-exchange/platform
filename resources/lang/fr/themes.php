3967c927a0eb028ad060bf4d73b114dc5bdc8c84
Ned Zimmerman <ned@bight.dev>
2021-09-13 12:53:23 -0300

    chore(l10n): translate themes.php into French, Canada

--- resources/lang/fr/themes.php
+++ resources/lang/fr/themes.php
@@ -4,5 +4,11 @@ return [
     'label_theme' => 'Thème de couleur',
     'system' => 'Thème du système',
     'light' => 'Thème clair',
-    'dark' => 'Thème sombre'
+    'dark' => 'Thème sombre',
+    'black-yellow' => 'Black on yellow theme',
+    'yellow-black' => 'Yellow on black theme',
+    'grey-dark' => 'Grey on dark grey theme',
+    'light-dark' => 'Light grey on dark grey theme',
+    'grey-white' => 'Grey on white theme',
+    'black-brown' => 'Black on brown theme'
 ];
