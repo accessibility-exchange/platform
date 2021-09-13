5d203f0a6777ddc3e3b63f124cfe5ce06c28f8a5
Ned Zimmerman <ned@bight.dev>
2021-09-13 12:39:22 -0300

    chore(l10n): translate invitation.php into Mohawk

---
+++ resources/lang/moh/invitation.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'invite_title' => 'Invite new member',
+    'invite_intro' => 'You can send an email invitation to someone who you would like to add to this team. If they do not have an account yet, they will be invited to create one first.',
+    'action_send_invitation' => 'Send invitation',
+    'cancel_member_invitation_link' => 'Cancel invitation',
+    'cancel_member_invitation_link_with_email' => 'Cancel invitation for :email',
+    'create_invitation_succeeded' => 'Your invitation has been sent.',
+    'invited_user_already_belongs_to_team' => 'This user already belongs to this team.',
+    'cancel_invitation_succeeded' => 'The invitation has been cancelled.',
+    'email_not_valid' => 'You are logged in as :email, but this invitation is for a different email address.',
+    'accept_invitation_succeeded' => 'You have joined the :inviteable team.',
+    'invitations_title' => 'Member invitations',
+    'invitation_email' => 'Email',
+    'invitation_status' => 'Status',
+    'invitation_role' => 'Role',
+    'member_invited' => 'Invited',
+];
