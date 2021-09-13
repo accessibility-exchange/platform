6745a1d2b0ae19f54077d2f021ef189aab5ad50e
Ned Zimmerman <ned@bight.dev>
2021-09-13 12:53:25 -0300

    chore(l10n): translate resource.php into French, Canada

--- resources/lang/fr/resource.php
+++ resources/lang/fr/resource.php
@@ -6,6 +6,9 @@ return [
     'label_language' => 'Langue de ressource',
     'label_summary' => 'Résumé de la ressource',
     'action_create' => 'Créer la ressource',
+    'index_title' => 'Resources',
+    'plural' => 'resources',
+    'singular' => 'resource',
     'edit_title' => 'Modifier la ressource',
     'edit_resource' => 'Modifier la ressource',
     'action_delete' => 'Supprimer la ressource',
@@ -14,4 +17,5 @@ return [
     'create_succeeded' => 'Votre ressource a été créée.',
     'update_succeeded' => 'Votre ressource a été mise à jour.',
     'destroy_succeeded' => 'Votre ressource a été supprimée.',
+    'none_found' => 'No resources found.',
 ];
