394e74df2037285f79c16b50b2c59d85d3575048
Ned Zimmerman <ned@bight.dev>
2021-09-13 12:38:43 -0300

    chore(l10n): translate dashboard.php into Inuktitut

---
+++ resources/lang/iu/dashboard.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'title' => 'Dashboard',
+    'your_title' => ':name’s dashboard',
+    'consultant_things_you_can_do' => 'Here are a few things to do before you can start consulting:',
+    'consultant_create_page_title' => 'Create your consultant page',
+    'consultant_create_page_info' => 'Tell us a little bit about yourself, so we can match you with an organization that suits you and your preferences.',
+    'consultant_learn_title' => 'Learn about consulting',
+    'consultant_learn_info' => 'Learn what you need to provide a great consulting service.',
+    'entity_things_you_can_do' => 'Here are a few things you can do:',
+    'entity_create_page_title' => 'Create a regulated entity page',
+    'entity_create_page_info' => 'Tell us a bit more about your organization.',
+    'entity_manage_page_title' => 'View or manage your regulated entity page',
+    'entity_learn_title' => 'Learn about engaging with the Deaf and disability community',
+    'entity_learn_info' => 'Learn what you need to make the consulting experience inclusive and accessible to Deaf people and people with disabilities through resources, trainings, and case studies.',
+    'entity_create_project_title' => 'Create a new project',
+    'entity_create_project_info' => 'Start a new accessibility project.',
+    'create_page_prompt' => 'Create your :item',
+    'learn_prompt' => 'Explore our resources',
+];
