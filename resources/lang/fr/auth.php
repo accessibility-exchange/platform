5b18cc195943743667d371ca18ed9aa1599815a0
Ned Zimmerman <ned@bight.dev>
2021-09-13 12:38:54 -0300

    chore(l10n): translate auth.php into French, Canada

--- resources/lang/fr/auth.php
+++ resources/lang/fr/auth.php
@@ -13,22 +13,8 @@ return [
     |
     */
 
-    'sign_in' => 'S’identifier',
-    'sign_out' => 'Se déconnecter',
-    'existing_account_prompt' => 'Vous avez déjà un compte?',
-    'label_password' => 'Mot de passe',
-    'label_password_confirmation' => 'Confirmez le mot de passe',
-    'label_remember_me' => 'Souvenez-vous de moi',
-    'error_intro' => 'Désolé, quelque chose s’est mal passé.',
     'failed' => 'Ces identifiants ne correspondent pas à nos enregistrements.',
-    'wrong_password' => 'Le mot de passe fourni est incorrect.',
+    'password' => 'The provided password is incorrect.',
     'throttle' => 'Trop de tentatives de connexion. Veuillez réessayer dans :seconds secondes.',
-    'confirm_intro' => 'Ceci est une zone sécurisée de l\'application. Veuillez confirmer votre mot de passe avant de continuer.',
-    'forget_prompt' => 'Mot de passe oublié?',
-    'forgot_intro' => 'Mot de passe oublié? Entrez votre adresse e-mail et nous vous enverrons par e-mail un lien de réinitialisation du mot de passe qui vous permettra d’en choisir un nouveau.',
-    'forgot_submit' => 'Envoyer le lien de réinitialisation',
-    'reset_submit' => 'Réinitialiser le mot de passe',
-    'verification_intro' => 'Merci pour votre inscription! Avant de commencer, pouvez-vous vérifier votre adresse e-mail en cliquant sur le lien que nous venons de vous envoyer? Si vous n’avez pas reçu l’e-mail, nous vous en enverrons un autre avec plaisir.',
-    'verification_sent' => 'Un nouveau lien de vérification a été envoyé à l’adresse e-mail que vous avez fournie lors de l’inscription. ',
-    'resend_verification_email' => 'Renvoyer l’e-mail de vérification'
+
 ];
