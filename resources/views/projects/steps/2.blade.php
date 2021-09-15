08528d6f98d1cbf6580a8191e85a3be570991ef6
Ned Zimmerman <ned@bight.dev>
2021-09-15 12:31:46 -0300

    feat: layout for substep completion/undo

--- resources/views/projects/steps/2.blade.php
+++ resources/views/projects/steps/2.blade.php
@@ -2,101 +2,117 @@
 
 <progress id="step" max="100" value="{{ count($project->progress[2]) / 4  * 100 }}"></progress>
 
-<ol role="list" class="flow">
-    <li>
-        <p>{{ __('Learn how to engage the Deaf and disability communities.') }}</p>
-        @if(!$project->isComplete($step, 1))
-        <form method="post" action="{{ localized_route('projects.update-progress', $project) }}">
-            @csrf
-            @method('PUT')
+<ol role="list" class="substeps flow">
+    <li class="substep">
+        <p class="substep__description">
+            {{ __('Learn how to engage the Deaf and disability communities.') }}<br />
+            <a href="{{ localized_route('resources.index') }}">{{ __('Browse resources in the Resource Hub') }}</a>
+        </p>
+        <div class="substep__progress">
+            @if(!$project->isComplete($step, 1))
+            <form method="post" action="{{ localized_route('projects.update-progress', $project) }}">
+                @csrf
+                @method('PUT')
 
-            <x-hearth-input type="hidden" name="step" :value="$step" />
-            <x-hearth-input type="hidden" name="substep" value="1" />
-            <x-hearth-button>{{ __('I’ve done this') }}</x-hearth-button>
-        </form>
-        @else
-        <p><x-heroicon-s-check-circle class="icon" width="24" height="24" /> Done</p>
-        <form method="post" action="{{ localized_route('projects.update-progress', $project) }}">
-            @csrf
-            @method('PUT')
+                <x-hearth-input type="hidden" name="step" :value="$step" />
+                <x-hearth-input type="hidden" name="substep" value="1" />
+                <x-hearth-button>{{ __('I’ve done this') }}</x-hearth-button>
+            </form>
+            @else
+            <p class="substep__progress__status"><x-heroicon-s-check-circle class="icon" /> Done</p>
+            <form method="post" action="{{ localized_route('projects.update-progress', $project) }}">
+                @csrf
+                @method('PUT')
 
-            <x-hearth-input type="hidden" name="step" :value="$step" />
-            <x-hearth-input type="hidden" name="substep" value="1" />
-            <x-hearth-input type="hidden" name="undo" value="1" />
-            <x-hearth-button>{{ __('Undo this step') }}</x-hearth-button>
-        </form>
-        @endif
+                <x-hearth-input type="hidden" name="step" :value="$step" />
+                <x-hearth-input type="hidden" name="substep" value="1" />
+                <x-hearth-input type="hidden" name="undo" value="1" />
+                <x-hearth-button>{{ __('Undo this step') }}</x-hearth-button>
+            </form>
+            @endif
+        </div>
     </li>
-    <li>
-        <p>{{ __('Think about how to structure your consultation.') }}</p>
-        @if(!$project->isComplete($step, 2))
-        <form method="post" action="{{ localized_route('projects.update-progress', $project) }}">
-            @csrf
-            @method('PUT')
+    <li class="substep">
+        <p class="substep__description">
+            {{ __('Think about how to structure your consultation.') }}<br />
+            <a href="{{ localized_route('resources.index') }}">{{ __('Browse case studies in the Resource Hub') }}</a>
+        </p>
+        <div class="substep__progress">
+            @if(!$project->isComplete($step, 2))
+            <form method="post" action="{{ localized_route('projects.update-progress', $project) }}">
+                @csrf
+                @method('PUT')
 
-            <x-hearth-input type="hidden" name="step" :value="$step" />
-            <x-hearth-input type="hidden" name="substep" value="2" />
-            <x-hearth-button>{{ __('I’ve done this') }}</x-hearth-button>
-        </form>
-        @else
-        <p><x-heroicon-s-check-circle class="icon" width="24" height="24" /> Done</p>
-        <form method="post" action="{{ localized_route('projects.update-progress', $project) }}">
-            @csrf
-            @method('PUT')
+                <x-hearth-input type="hidden" name="step" :value="$step" />
+                <x-hearth-input type="hidden" name="substep" value="2" />
+                <x-hearth-button>{{ __('I’ve done this') }}</x-hearth-button>
+            </form>
+            @else
+            <p class="substep__progress__status"><x-heroicon-s-check-circle class="icon" /> Done</p>
+            <form method="post" action="{{ localized_route('projects.update-progress', $project) }}">
+                @csrf
+                @method('PUT')
 
-            <x-hearth-input type="hidden" name="step" :value="$step" />
-            <x-hearth-input type="hidden" name="substep" value="2" />
-            <x-hearth-input type="hidden" name="undo" value="1" />
-            <x-hearth-button>{{ __('Undo this step') }}</x-hearth-button>
-        </form>
-        @endif
+                <x-hearth-input type="hidden" name="step" :value="$step" />
+                <x-hearth-input type="hidden" name="substep" value="2" />
+                <x-hearth-input type="hidden" name="undo" value="1" />
+                <x-hearth-button>{{ __('Undo this step') }}</x-hearth-button>
+            </form>
+            @endif
+        </div>
     </li>
-    <li>
-        <p>{{ __('Learn about how the disability community is currently engaging with your organization or sector.') }}</p>
-        @if(!$project->isComplete($step, 3))
-        <form method="post" action="{{ localized_route('projects.update-progress', $project) }}">
-            @csrf
-            @method('PUT')
-
-            <x-hearth-input type="hidden" name="step" :value="$step" />
-            <x-hearth-input type="hidden" name="substep" value="3" />
-            <x-hearth-button>{{ __('I’ve done this') }}</x-hearth-button>
-        </form>
-        @else
-        <p><x-heroicon-s-check-circle class="icon" width="24" height="24" /> Done</p>
-        <form method="post" action="{{ localized_route('projects.update-progress', $project) }}">
-            @csrf
-            @method('PUT')
-
-            <x-hearth-input type="hidden" name="step" :value="$step" />
-            <x-hearth-input type="hidden" name="substep" value="3" />
-            <x-hearth-input type="hidden" name="undo" value="1" />
-            <x-hearth-button>{{ __('Undo this step') }}</x-hearth-button>
-        </form>
-        @endif
+    <li class="substep">
+        <p class="substep__description">
+            {{ __('Learn about how the disability community is currently engaging with your organization or sector.') }}<br />
+            <a href="{{ localized_route('resources.index') }}">{{ __('Browse stories in the Resource Hub') }}</a>
+        </p>
+        <div class="substep__progress">
+            @if(!$project->isComplete($step, 3))
+            <form method="post" action="{{ localized_route('projects.update-progress', $project) }}">
+                @csrf
+                @method('PUT')
+                <x-hearth-input type="hidden" name="step" :value="$step" />
+                <x-hearth-input type="hidden" name="substep" value="3" />
+                <x-hearth-button>{{ __('I’ve done this') }}</x-hearth-button>
+            </form>
+            @else
+            <p class="substep__progress__status"><x-heroicon-s-check-circle class="icon" /> Done</p>
+            <form method="post" action="{{ localized_route('projects.update-progress', $project) }}">
+                @csrf
+                @method('PUT')
+                <x-hearth-input type="hidden" name="step" :value="$step" />
+                <x-hearth-input type="hidden" name="substep" value="3" />
+                <x-hearth-input type="hidden" name="undo" value="1" />
+                <x-hearth-button>{{ __('Undo this step') }}</x-hearth-button>
+            </form>
+            @endif
+        </div>
     </li>
-    <li>
-        <p>{{ __('Learn about different ways to meet access needs.') }}</p>
-        @if(!$project->isComplete($step, 4))
-        <form method="post" action="{{ localized_route('projects.update-progress', $project) }}">
-            @csrf
-            @method('PUT')
-
-            <x-hearth-input type="hidden" name="step" :value="$step" />
-            <x-hearth-input type="hidden" name="substep" value="4" />
-            <x-hearth-button>{{ __('I’ve done this') }}</x-hearth-button>
-        </form>
-        @else
-        <p><x-heroicon-s-check-circle class="icon" width="24" height="24" /> Done</p>
-        <form method="post" action="{{ localized_route('projects.update-progress', $project) }}">
-            @csrf
-            @method('PUT')
-
-            <x-hearth-input type="hidden" name="step" :value="$step" />
-            <x-hearth-input type="hidden" name="substep" value="4" />
-            <x-hearth-input type="hidden" name="undo" value="1" />
-            <x-hearth-button>{{ __('Undo this step') }}</x-hearth-button>
-        </form>
-        @endif
+    <li class="substep">
+        <p class="substep__description">
+            {{ __('Learn about different ways to meet access needs.') }}<br />
+            <a href="{{ localized_route('resources.index') }}">{{ __('Browse resources in the Resource Hub') }}</a>
+        </p>
+        <div class="substep__progress">
+            @if(!$project->isComplete($step, 4))
+            <form method="post" action="{{ localized_route('projects.update-progress', $project) }}">
+                @csrf
+                @method('PUT')
+                <x-hearth-input type="hidden" name="step" :value="$step" />
+                <x-hearth-input type="hidden" name="substep" value="4" />
+                <x-hearth-button>{{ __('I’ve done this') }}</x-hearth-button>
+            </form>
+            @else
+            <p class="substep__progress__status"><x-heroicon-s-check-circle class="icon" /> Done</p>
+            <form method="post" action="{{ localized_route('projects.update-progress', $project) }}">
+                @csrf
+                @method('PUT')
+                <x-hearth-input type="hidden" name="step" :value="$step" />
+                <x-hearth-input type="hidden" name="substep" value="4" />
+                <x-hearth-input type="hidden" name="undo" value="1" />
+                <x-hearth-button>{{ __('Undo this step') }}</x-hearth-button>
+            </form>
+            @endif
+        </div>
     </li>
-</ul>
+</ol>
