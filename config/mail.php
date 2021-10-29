cb71fe5a440427b4d1ed70be1447251b97497aa3
Ned Zimmerman <ned@bight.dev>
2021-10-29 08:55:51 -0300

    feat: rename to The Accessibility Exchange (#188)

--- config/mail.php
+++ config/mail.php
@@ -85,7 +85,7 @@ return [
 
     'from' => [
         'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', env('APP_NAME', 'Accessibility in Action')),
+        'name' => env('MAIL_FROM_NAME', env('APP_NAME', 'The Accessibility Exchange')),
     ],
 
     /*
