cacf2a6aaa38e38fd24bdc38025bb72cb6221c89
Ned Zimmerman <ned@bight.dev>
2021-11-17 16:40:08 -0400

    fix: correct English route

--- resources/lang/en/routes.php
+++ resources/lang/en/routes.php
@@ -6,7 +6,7 @@ return [
     'login' => 'login',
     'register' => 'register',
     'verification.verify' => '/verify-email/{id}/{hash}',
-    'projects' => 'projets',
+    'projects' => 'projects',
     'projects/{project}' => 'projects/{project}',
     'entities/{entity}/accessibility-and-inclusion' => 'entities/{entity}/accessibility-and-inclusion',
     'entities/{entity}/projects' => 'entities/{entity}/projects',
