ce567051da828a576205cbd61fb7655e999cccc3
Ned Zimmerman <ned@bight.dev>
2021-11-17 16:06:36 -0400

    feat: clarified badge behaviour, added component defaults, implemented translatable project editor

---
+++ app/View/Components/TranslatableInput.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\View\Components;
+
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class TranslatableInput extends Component
+{
+    /**
+     * The name for the input.
+     *
+     * @var string
+     */
+    public string $name;
+
+    /**
+     * The label for the input.
+     *
+     * @var string
+     */
+    public string $label;
+
+    /**
+     * The locales supported by the input.
+     *
+     * @var array
+     */
+    public array $locales;
+
+    /**
+     * The model to which the input field belongs.
+     *
+     * @var mixed
+     */
+    public mixed $model;
+
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public function __construct($name, $label, $locales = null, $model = null)
+    {
+        $this->name = $name;
+        $this->label = $label;
+        $this->locales = $locales ?? config('locales.supported');
+        $this->model = $model;
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View
+     */
+    public function render(): View
+    {
+        return view('components.translatable-input');
+    }
+}
--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -12,26 +12,23 @@
 
     <p>{{ __('This project will be created on behalf of :entity.', ['entity' => $entity->name]) }}</p>
 
-    {{-- TODO: Remove this --}}
-    @php $locales = ['en', 'fr']; @endphp
-
     <form id="create-project" action="{{ localized_route('projects.store', $entity) }}" method="POST" novalidate>
         @csrf
         <x-hearth-input id="entity_id" type="hidden" name="entity_id" :value="$entity->id" required />
 
-        <x-translatable-input name="name" :label="__('Project name')" :locales="$locales" />
+        <x-translatable-input name="name" :label="__('Project name')" />
 
         <x-hearth-date-input :label="__('Project start date')" name="start_date" :value="old('start_date', '')" />
 
         <x-hearth-date-input :label="__('Project end date')" name="end_date" :value="old('end_date', '')" />
 
-        <x-translatable-textarea name="goals" :label="__('Goals for consultation')" :locales="$locales" />
+        <x-translatable-textarea name="goals" :label="__('Goals for consultation')" />
 
-        <x-translatable-textarea name="impact" :label="__('Who will the project impact')" :locales="$locales" />
+        <x-translatable-textarea name="impact" :label="__('Who will the project impact')" />
 
-        <x-translatable-textarea name="out_of_scope" :label="__('What is this project not going to do?')" :locales="$locales" />
+        <x-translatable-textarea name="out_of_scope" :label="__('What is this project not going to do?')" />
 
-        <x-translatable-textarea name="timeline" :label="__('Project timeline')" :locales="$locales" />
+        <x-translatable-textarea name="timeline" :label="__('Project timeline')" />
 
         <x-hearth-button>{{ __('Create project') }}</x-hearth-button>
     </form>
