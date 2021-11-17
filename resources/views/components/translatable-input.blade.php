5a073a0bcaff2d452a9c0202d4060267e18df7c4
Ned Zimmerman <ned@bight.dev>
2021-11-17 08:00:40 -0400

    fix: improve badge semantics

--- resources/views/components/translatable-input.blade.php
+++ resources/views/components/translatable-input.blade.php
@@ -7,10 +7,11 @@
     @if($locale !== locale())
     <div class="expander field @error($name . '.' . $locale) field--error @enderror" x-data="{expanded: false, value: '{{ old($name . '.' . $locale, '') }}'}">
         <p class="expander__summary" id="{{ Str::slug(__(':label (:locale)', ['label' => $label, 'locale' => get_locale_name($locale)])) }}">
-            <button type="button" x-bind:aria-expanded="expanded.toString()" x-on:click="expanded = !expanded">
-                {{ __(':label (:locale)', ['label' => $label, 'locale' => get_locale_name($locale)]) }} <span class="badge" x-show="value" x-text="value ? '{{ __('Content added') }}' : ''"></span> <x-heroicon-s-plus x-show="! expanded" aria-hidden="true" class="icon" /><x-heroicon-s-minus x-show="expanded" aria-hidden="true" class="icon" />
+            <button type="button" x-bind:aria-expanded="expanded.toString()" x-on:click="expanded = !expanded" aria-describedby="{{ Str::slug(__(':label (:locale)', ['label' => $label, 'locale' => get_locale_name($locale)])) }}-status">
+                {{ __(':label (:locale)', ['label' => $label, 'locale' => get_locale_name($locale)]) }} <x-heroicon-s-plus x-show="! expanded" aria-hidden="true" class="icon" /><x-heroicon-s-minus x-show="expanded" aria-hidden="true" class="icon" />
             </button>
         </p>
+        <span class="badge" id="{{ Str::slug(__(':label (:locale)', ['label' => $label, 'locale' => get_locale_name($locale)])) }}-status" x-show="value" x-text="value ? '{{ __('Content added') }}' : ''"></span>
         <div class="expander__content" x-show="expanded">
             <x-hearth-input type="text" :id="$name . '_' . $locale" :name="$name . '[' . $locale . ']'" :value="old($name . '.' . $locale, '')" x-model="value" :aria-labelledby="Str::slug(__(':label (:locale)', ['label' => $label, 'locale' => get_locale_name($locale)]))" />
             <x-hearth-error :for="$name . '.' . $locale" />
--- resources/views/components/translatable-textarea.blade.php
+++ resources/views/components/translatable-textarea.blade.php
@@ -5,14 +5,15 @@
 </div>
 @foreach($locales as $locale)
     @if($locale !== locale())
-    <div class="expander field @error($name . '.' . $locale) field--error @enderror" x-data="{expanded: false}">
+    <div class="expander field @error($name . '.' . $locale) field--error @enderror" x-data="{expanded: false, value: '{{ old($name . '.' . $locale, '') }}'}">
         <p class="expander__summary" id="{{ Str::slug(__(':label (:locale)', ['label' => $label, 'locale' => get_locale_name($locale)])) }}">
-            <button type="button" x-bind:aria-expanded="expanded.toString()" x-on:click="expanded = !expanded">
+            <button type="button" x-bind:aria-expanded="expanded.toString()" x-on:click="expanded = !expanded" aria-describedby="{{ Str::slug(__(':label (:locale)', ['label' => $label, 'locale' => get_locale_name($locale)])) }}-status">
                 {{ __(':label (:locale)', ['label' => $label, 'locale' => get_locale_name($locale)]) }} <x-heroicon-s-plus x-show="! expanded" aria-hidden="true" class="icon" /><x-heroicon-s-minus x-show="expanded" aria-hidden="true" class="icon" />
             </button>
         </p>
+        <span class="badge" id="{{ Str::slug(__(':label (:locale)', ['label' => $label, 'locale' => get_locale_name($locale)])) }}-status" x-show="value" x-text="value ? '{{ __('Content added') }}' : ''"></span>
         <div class="expander__content" x-show="expanded">
-            <x-hearth-textarea :id="$name . '_' . $locale" :name="$name . '[' . $locale . ']'" :value="old($name . '.' . $locale, '')" :aria-labelledby="Str::slug(__(':label (:locale)', ['label' => $label, 'locale' => get_locale_name($locale)]))" />
+            <x-hearth-textarea :id="$name . '_' . $locale" :name="$name . '[' . $locale . ']'" :value="old($name . '.' . $locale, '')" x-model="value" :aria-labelledby="Str::slug(__(':label (:locale)', ['label' => $label, 'locale' => get_locale_name($locale)]))" />
             <x-hearth-error :for="$name . '.' . $locale" />
         </div>
     </div>
