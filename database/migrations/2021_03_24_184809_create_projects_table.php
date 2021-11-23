bab47537ddd020ba2079b2aad553d560f924b282
Ned Zimmerman <ned@bight.dev>
2021-11-23 10:34:39 -0400

    feat: consolidate project migrations

--- database/migrations/2021_03_24_184809_create_projects_table.php
+++ database/migrations/2021_03_24_184809_create_projects_table.php
@@ -15,7 +15,7 @@ class CreateProjectsTable extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->json('name');
             $table->string('slug');
             $table->date('start_date');
             $table->date('end_date')->nullable();
