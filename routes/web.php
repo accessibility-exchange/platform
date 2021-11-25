6c3f3cd3d1d778baaac3d113c4f046a658fd18c0
Ned Zimmerman <ned@bight.dev>
2021-11-25 14:40:25 -0400

    feat: split dashboard view into two partials

--- routes/web.php
+++ routes/web.php
@@ -19,9 +19,9 @@ Route::multilingual('/', function () {
     return view('welcome');
 })->name('welcome');
 
-Route::multilingual('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+Route::multilingual('/dashboard', [UserController::class, 'dashboard'])
+    ->middleware(['auth'])
+    ->name('dashboard');
 
 Route::multilingual('/account/edit', [UserController::class, 'edit'])
     ->middleware(['auth'])
