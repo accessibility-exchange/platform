47e55406771be14221878eef911c124fdfcc8bda
Ned Zimmerman <ned@bight.dev>
2021-12-03 15:23:09 -0400

    feat: use standard layout for registration

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,44 +1,38 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="{{ localized_route('welcome') }}">
-                {{ config('app.name', 'The Accessibility Exchange') }}
-            </a>
-        </x-slot>
+<x-app-layout>
+    <x-slot name="header">
+        <h1>
+        @switch(request()->get('step'))
+            @case(1)
+            {{ __('Your role') }}
+            @break
+            @case(2)
+            {{ __('Your details') }}
+            @break
+            @case(3)
+            {{ __('Choose a password') }}
+            @break
+            @default
+            {{ __('Create an account') }}
+        @endswitch
+        </h1>
+    </x-slot>
 
-        <x-slot name="title">
-            @switch(request()->get('step'))
-                @case(1)
-                    {{ __('Your role') }}
-                    @break
-                @case(2)
-                    {{ __('Your details') }}
-                    @break
-                @case(3)
-                    {{ __('Choose a password') }}
-                    @break
-                @default
-                {{ __('Create an account') }}
-            @endswitch
-        </x-slot>
+    <!-- Validation Errors -->
+    @if ($errors->any())
+    <x-hearth-alert type="error">
+        <p>{{ __('hearth::auth.error_intro') }}</p>
+    </x-hearth-alert>
+    @endif
 
-        <!-- Validation Errors -->
-        @if ($errors->any())
-            <x-hearth-alert type="error">
-                <p>{{ __('hearth::auth.error_intro') }}</p>
-            </x-hearth-alert>
-        @endif
+    @if(request()->get('step'))
+        @include('auth.register.steps.' . request()->get('step'))
+    @else
+        @include('auth.register.steps.0')
+    @endif
 
-        @if(request()->get('step'))
-            @include('auth.register.steps.' . request()->get('step'))
-        @else
-            @include('auth.register.steps.0')
-        @endif
-
-        <p>
-            {{ __('Already have an account?') }} <a href="{{ localized_route('login') }}">
-                {{ __('Sign in.') }}
-            </a>
-        </p>
-    </x-auth-card>
-</x-guest-layout>
+    <p>
+        {{ __('Already have an account?') }} <a href="{{ localized_route('login') }}">
+            {{ __('Sign in.') }}
+        </a>
+    </p>
+</x-app-layout>
