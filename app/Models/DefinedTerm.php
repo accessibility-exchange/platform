58c0c20ecd486c7bd3cd3f9d27309f2c6d1eebc3
Ned Zimmerman <ned@bight.dev>
2022-03-16 15:55:25 -0600

    feat: add basic glossary model and controllers

---
+++ app/Models/DefinedTerm.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Translatable\HasTranslations;
+
+class DefinedTerm extends Model
+{
+    use HasFactory;
+    use HasTranslations;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'term',
+        'definition',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'term' => 'array',
+        'definition' => 'array',
+    ];
+
+    /**
+     * The attributes that are translatable.
+     *
+     * @var array
+     */
+    public array $translatable = [
+        'term',
+        'definition',
+    ];
+}
