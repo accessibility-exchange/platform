5a450bedf3cb94a0c4a2b4ab372f8a8ac5228483
Ned Zimmerman <ned@bight.dev>
2022-04-12 12:44:13 -0300

    feat: use checkbox for password input visibility
    Resolves #351.

--- resources/views/components/password-input.blade.php
+++ resources/views/components/password-input.blade.php
@@ -3,12 +3,10 @@
     'autocomplete' => false
 ])
 
-<div class="password repel" x-data="{show: false}">
+<div class="password" x-data="{show: false}">
     <input name="{{ $name }}" id="{{ $id ?? $name }}" x-bind:type="show ? 'text' : 'password'" required {!! $autocomplete ? 'autocomplete="' . $autocomplete . '"' : '' !!} />
-    <button type="button" x-on:click="show = !show" x-bind:aria-pressed="show">
-        <x-heroicon-o-eye x-show="!show" />
-        <x-heroicon-o-eye-off x-show="show" x-cloak />
-        <span x-show="!show" />{{ __('Show') }}</span>
-        <span x-show="show" x-cloak />{{ __('Hide') }}</span>
-    </button>
+    <div class="field">
+        <x-hearth-checkbox :name="'show_' . $name" x-model="show" />
+        <x-hearth-label :for="'show_' . $name" :value="__('Show password')" />
+    </div>
 </div>
