b850a1b918fa46c23cb5546b139141ecd5171e7f
Ned Zimmerman <ned@bight.dev>
2022-04-13 14:45:06 -0300

    fix: allow hints for password inputs (fix #451)

--- resources/views/components/password-input.blade.php
+++ resources/views/components/password-input.blade.php
@@ -1,12 +1,18 @@
-@props([
-    'name' => 'password',
-    'autocomplete' => false
-])
-
 <div class="password" x-data="{show: false}">
-    <input name="{{ $name }}" id="{{ $id ?? $name }}" x-bind:type="show ? 'text' : 'password'" required {!! $autocomplete ? 'autocomplete="' . $autocomplete . '"' : '' !!} />
+    <input type="password" x-bind:type="show ? 'text' : 'password'"
+        {!! $attributes->merge([
+            'name' => $name,
+            'id' => $id,
+        ]) !!}
+        {{ $required ? 'required' : '' }}
+        @disabled($disabled)
+        {!! $describedBy() ? 'aria-describedby="' . $describedBy() . '"' : '' !!}
+        {!! $invalid ? 'aria-invalid="true"' : '' !!}
+    >
     <div class="field">
         <x-hearth-checkbox :name="'show_' . $name" x-model="show" />
         <x-hearth-label :for="'show_' . $name" :value="__('Show password')" />
     </div>
 </div>
+
+
