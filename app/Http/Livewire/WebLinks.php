9b3e33b3a2ef236b08d1cfd385877444dbba4ffc
Ned Zimmerman <ned@bight.dev>
2022-05-02 17:20:18 -0300

    feat: generalize WebLinks component

--- app/Http/Livewire/WebLinks.php
+++ app/Http/Livewire/WebLinks.php
@@ -2,17 +2,29 @@
 
 namespace App\Http\Livewire;
 
+use Illuminate\Contracts\View\View;
 use Livewire\Component;
 
 class WebLinks extends Component
 {
-    public $links = [];
+    public string $name;
 
-    public function mount(array $links)
+    public array $links;
+
+    /**
+     * @param array $links
+     *
+     * @return void
+     */
+    public function mount(array $links): void
     {
-        $this->links = old('web_links', $links);
+        $this->name = $name ?? 'web_links';
+        $this->links = old($this->name, $links);
     }
 
+    /**
+     * @return void
+     */
     public function addLink(): void
     {
         if (! $this->canAddMoreLinks()) {
@@ -22,6 +34,11 @@ class WebLinks extends Component
         $this->links[] = ['title' => '', 'url' => ''];
     }
 
+    /**
+     * @param int $i The index of the link to remove.
+     *
+     * @return void
+     */
     public function removeLink(int $i): void
     {
         unset($this->links[$i]);
@@ -29,12 +46,18 @@ class WebLinks extends Component
         $this->links = array_values($this->links);
     }
 
-    public function canAddMoreLinks()
+    /**
+     * @return bool
+     */
+    public function canAddMoreLinks(): bool
     {
         return count($this->links) < 5;
     }
 
-    public function render()
+    /**
+     * @return View
+     */
+    public function render(): View
     {
         return view('livewire.web-links');
     }
--- resources/views/livewire/web-links.blade.php
+++ resources/views/livewire/web-links.blade.php
@@ -2,22 +2,22 @@
     <ul role="list" class="stack">
         @foreach($links as $i => $link)
         <li class="stack">
-            <div class="field @error("web_links.{$i}.title") field--error @enderror">
-                <x-hearth-label :for="'web_links_title_' . $i" :value="__('Website title')" />
-                <x-hearth-input :id="'web_links_title_' . $i" :name="'web_links[' . $i . '][title]'" :value="$link['title']" />
-                <x-hearth-error :for="'web_links_' . $i . '_title'" :field="'web_links.' . $i . '.title'" />
+            <div class="field @error("{$name}.{$i}.title") field--error @enderror">
+                <x-hearth-label :for="$name . '_title_' . $i" :value="__('Website title')" />
+                <x-hearth-input :id="$name . '_title_' . $i" :name="$name . '[' . $i . '][title]'" :value="$link['title']" />
+                <x-hearth-error :for="$name . '_' . $i . '_title'" :field="$name . '.' . $i . '.title'" />
             </div>
-            <div class="field @error("web_links.{$i}.url") field--error @enderror">
-                <x-hearth-label :for="'web_links_url_' . $i" :value="__('Website link')" />
-                <x-hearth-input type="url" :id="'url_' . $i" :name="'web_links[' . $i . '][url]'" :value="$link['url']" />
-                <x-hearth-error :for="'web_links_' . $i . '_url'" :field="'web_links.' . $i . '.url'" />
+            <div class="field @error("{$name}.{$i}.url") field--error @enderror">
+                <x-hearth-label :for="$name . '_url_' . $i" :value="__('Website link')" />
+                <x-hearth-input type="url" :id="'url_' . $i" :name="$name . '[' . $i . '][url]'" :value="$link['url']" />
+                <x-hearth-error :for="$name . '_' . $i . '_url'" :field="$name . '.' . $i . '.url'" />
             </div>
             @if($loop->count > 1)
-            <button class="secondary" type="button" wire:click="removeLink({{ $i }})">{{ __('Remove this website') }}</button>
+            <button class="secondary" type="button" wire:click="removeLink({{ $i }})">{{ __('Remove this link') }}</button>
             @endif
         @endforeach
     </ul>
     @if ($this->canAddMoreLinks())
-    <button class="secondary" type="button" wire:click="addLink">{{ __('Add another website') }}</button>
+    <button class="secondary" type="button" wire:click="addLink">{{ __('Add another link') }}</button>
     @endif
 </div>
