f0433d8526b233efbf77fd9227e9527b6f335d89
Ned Zimmerman <ned@bight.dev>
2022-05-24 10:46:46 -0400

    fix: restore factories

--- database/factories/OrganizationFactory.php
+++ database/factories/OrganizationFactory.php
@@ -3,8 +3,6 @@
 namespace Database\Factories;
 
 use App\Models\Organization;
-use App\Models\RegulatedOrganization;
-use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class OrganizationFactory extends Factory
@@ -28,17 +26,5 @@ class OrganizationFactory extends Factory
             'locality' => $this->faker->city(),
             'region' => $this->faker->provinceAbbr(),
         ];
-    }
-
-    /**
-     * Configure the model factory.
-     *
-     * @return $this
-     */
-    public function configure(): static
-    {
-        return $this->afterCreating(function (Organization $organization) {
-            $organization->users()->attach(User::factory()->create(['context' => 'regulated-organization']), ['role' => 'admin']);
-        });
     }
 }
--- database/factories/RegulatedOrganizationFactory.php
+++ database/factories/RegulatedOrganizationFactory.php
@@ -3,7 +3,6 @@
 namespace Database\Factories;
 
 use App\Models\RegulatedOrganization;
-use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class RegulatedOrganizationFactory extends Factory
@@ -26,17 +25,5 @@ class RegulatedOrganizationFactory extends Factory
             'name' => $this->faker->company(),
             'type' => $this->faker->randomElement(['government', 'business', 'public-sector']),
         ];
-    }
-
-    /**
-     * Configure the model factory.
-     *
-     * @return $this
-     */
-    public function configure(): static
-    {
-        return $this->afterCreating(function (RegulatedOrganization $regulatedOrganization) {
-            $regulatedOrganization->users()->attach(User::factory()->create(['context' => 'regulated-organization']), ['role' => 'admin']);
-        });
     }
 }
