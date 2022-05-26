d38dcfbf5560a1478d247b502835fe7c7014aa1c
Ned Zimmerman <ned@bight.dev>
2022-05-26 16:31:27 -0300

    chore(localization): translate membership.php into French, Canada

--- resources/lang/fr/membership.php
+++ resources/lang/fr/membership.php
@@ -4,5 +4,5 @@ return [
     'role_update_succeeded' => 'Le rôle de :user a été mis à jour.',
     'remove_member_succeeded' => ':user a été enlevé de :membershipable.',
     'edit_user_role_title' => 'Modifier le rôle de :user',
-    'edit_user_role_intro' => 'La modification du rôle changera les privilèges de :user pour leur adhésion.',
+    'edit_user_role_intro' => 'La modification du rôle changera les privilèges de :user pour leur :membershipable adhésion.',
 ];
