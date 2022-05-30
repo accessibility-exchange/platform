ea61bb7c6e2cec56854e939720e5f89d7c00c14f
Ned Zimmerman <ned@bight.dev>
2022-05-30 16:24:30 -0300

    feat: language settings on RegulatedOrganization show view

--- resources/views/regulated-organizations/show.blade.php
+++ resources/views/regulated-organizations/show.blade.php
@@ -27,13 +27,9 @@
         </div>
     </x-slot>
 
-
-    @if(Auth::user()->hasRequestedToJoin($regulatedOrganization))
-        <form action="{{ localized_route('requests.cancel') }}" method="POST">
-            @csrf
-            <button>{{ __('Cancel request to join :regulated-organization', ['regulated-organization' => $regulatedOrganization->name]) }}</button>
-        </form>
-    @endif
+    @can('update', $regulatedOrganization)
+    <x-translation-manager :model="$regulatedOrganization" />
+    @endcan
 
     <div class="with-sidebar">
         <nav class="secondary" aria-labelledby="regulated-organization">
