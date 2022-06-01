8ee00bee2279c2f0411da5b04a77bceed40a14db
Ned Zimmerman <ned@bight.dev>
2022-05-31 21:04:44 -0300

    chore(localization): update translations (#514)
    * chore(localization): translate project.php into French, Canada
    
    * chore(localization): translate regulated-organization.php into French, Canada

---
+++ resources/lang/fr/project.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'statuses' => [
+        'upcoming' => 'Upcoming',
+        'current' => 'In progress',
+        'completed' => 'Completed',
+        'cancelled' => 'Cancelled',
+        'seeking-participants' => 'Seeking participants',
+        'has-community-connector' => 'Has community connector',
+    ],
+];
