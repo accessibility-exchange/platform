6851edb26d5c9bb66466b67fb214820082d854f5
Ned Zimmerman <ned@bight.dev>
2022-06-16 14:12:56 -0300

    chore(localization): translate routes.php into French, Canada

--- resources/lang/fr/routes.php
+++ resources/lang/fr/routes.php
@@ -6,4 +6,8 @@ return [
     'login' => 'se-connecter',
     'register' => 'inscription',
     'verification.verify' => '/verification-de-courriel/{id}/{hash}',
+    'projects' => 'projets',
+    'projects/{project}' => 'projets/{project}',
+    'entities/{regulatedOrganization}/accessibility-and-inclusion' => 'entites/{regulatedOrganization}/accessibilite-et-inclusion',
+    'entities/{regulatedOrganization}/projects' => 'entites/{regulatedOrganization}/projets',
 ];
