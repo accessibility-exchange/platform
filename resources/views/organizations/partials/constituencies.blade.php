5336253606e7c160e69ba97ddf80935541e2aac2
Ned Zimmerman <ned@bight.dev>
2022-06-20 21:10:48 -0300

    feat: update constituency groupings (resolves #535)

--- resources/views/organizations/partials/constituencies.blade.php
+++ resources/views/organizations/partials/constituencies.blade.php
@@ -1,53 +1,47 @@
-<h3>{{ __('People with disabilities, Deaf persons, and/or supporters') }}</h3>
+<h3>{{ __('Groups in the disability and Deaf community') }}</h3>
 
 <ul role="list" class="tags">
 @foreach($organization->livedExperiences as $livedExperience)
     <li class="tag">{{ $livedExperience->name }}</li>
 @endforeach
+@foreach($organization->disabilityTypes as $disabilityType)
+    <li class="tag">{{ $disabilityType->name }}</li>
+@endforeach
+@if($organization->other_disability_type)
+    <li class="tag">{{ $organization->other_disability_type }}</li>
+@endif
 </ul>
 
-@if(in_array(1, $organization->livedExperiences->pluck('id')->toArray()))
-<h3>{{ __('Disabilities') }}</h3>
+@if($organization->extra_attributes->has_indigenous_identities || $organization->extra_attributes->has_ethnoracial_identities)
+<h3>{{ __('Ethno-racial groups') }}</h3>
 
 <ul role="list" class="tags">
-    @foreach($organization->disabilityTypes as $disabilityType)
-        <li class="tag">{{ $disabilityType->name }}</li>
+    @foreach($organization->indigenousIdentities as $indigenousIdentity)
+    <li class="tag">{{ $indigenousIdentity->name }}</li>
     @endforeach
-    @if($organization->other_disability_type)
-        <li class="tag">{{ $organization->other_disability_type }}</li>
-    @endif
+    @foreach($organization->ethnoracialIdentities as $ethnoracialIdentity)
+    <li class="tag">{{ $ethnoracialIdentity->name }}</li>
+    @endforeach
 </ul>
 @endif
 
-@if($organization->extra_attributes->has_indigenous_identities)
-    <h3>{{ __('Indigenous identities') }}</h3>
-
-    <ul role="list" class="tags">
-        @foreach($organization->indigenousIdentities as $indigenousIdentity)
-            <li class="tag">{{ $indigenousIdentity->name }}</li>
-        @endforeach
-    </ul>
-@endif
-
-@if($organization->extra_attributes->has_gender_and_sexual_identities)
-<h3>{{ __('Gender and sexual identities') }}</h3>
+@if($organization->extra_attributes->has_gender_and_sexual_identities || $organization->extra_attributes->has_refugee_and_immigrant_constituency)
+<h3>{{ __('Other identity groups') }}</h3>
 
 <ul role="list" class="tags">
+    @if($organization->extra_attributes->has_refugee_and_immigrant_constituency)
+    <li class="tag">{{ __('Refugees and/or immigrants') }}</li>
+    @endif
     @foreach($organization->genderIdentities as $genderIdentity)
-        <li class="tag">{{ $genderIdentity->name_plural }}</li>
+    <li class="tag">{{ $genderIdentity->name_plural }}</li>
     @endforeach
     @if($organization->constituencies->contains($transPeople))
-        <li class="tag">{{ $transPeople->name_plural }}</li>
+    <li class="tag">{{ $transPeople->name_plural }}</li>
     @endif
     @if($organization->constituencies->contains($twoslgbtqiaplusPeople))
-        <li class="tag">{{ $twoslgbtqiaplusPeople->name_plural }}</li>
+    <li class="tag">{{ $twoslgbtqiaplusPeople->name_plural }}</li>
     @endif
 </ul>
-@endif
-
-@if($organization->extra_attributes->has_refugee_and_immigrant_constituency)
-<h3>{{ __('Refugees and/or immigrants') }}</h3>
-<ul role="list" class="tags"><li class="tag">{{ __('Yes') }}</li></ul>
 @endif
 
 @if($organization->extra_attributes->has_age_brackets)
@@ -60,18 +54,8 @@
 </ul>
 @endif
 
-@if($organization->extra_attributes->has_ethnoracial_identities)
-    <h3>{{ __('Ethno-racial identities') }}</h3>
-
-    <ul role="list" class="tags">
-        @foreach($organization->ethnoracialIdentities as $ethnoracialIdentity)
-            <li class="tag">{{ $ethnoracialIdentity->name }}</li>
-        @endforeach
-    </ul>
-@endif
-
 @if($organization->constituentLanguages->count() > 0)
-<h3>{{ __('Languages') }}</h3>
+<h3>{{ __('Language groups') }}</h3>
 
 <ul role="list" class="tags">
     @foreach($organization->constituentLanguages as $language)
