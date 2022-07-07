6e55f645d32c3282280f52a0758a0de67337e755
Ned Zimmerman <ned@bight.dev>
2022-07-06 21:29:42 -0600

    fix: don’t show role change warning unless needed

--- resources/views/individuals/show-role-edit.blade.php
+++ resources/views/individuals/show-role-edit.blade.php
@@ -10,15 +10,17 @@
 
     <p>{{ __('You can always change this later.') }} <a href="{{ localized_route('about.for-individuals') }}">{{ __('Learn more about these roles') }}</a></p>
 
-    <form class="stack" action="{{ localized_route('individuals.save-roles') }}" method="post" novalidate x-data="{roles: {{ old('roles', json_encode($selectedRoles) ?? '[]') }}}">
+    <form class="stack" action="{{ localized_route('individuals.save-roles') }}" method="post" novalidate x-data="{initialRoles: {{ json_encode($selectedRoles) }}, roles: {{ old('roles', json_encode($selectedRoles) ?? '[]') }}}">
         <fieldset class="field @error('roles') field--error @enderror">
-            <x-hearth-checkboxes name="roles" :options="$roles" :checked="old('roles', $selectedRoles ?? [])" x-model="roles" />
+            <x-hearth-checkboxes name="roles" :options="$roles" :checked="old('roles', $selectedRoles ?? [])" x-model.numbe="roles" />
             <x-hearth-error for="roles" />
         </fieldset>
 
-        <x-hearth-alert type="warning" x-show="!roles.includes(2) && !roles.includes('2') && !roles.includes(3) && !roles.includes('3')">
-            {{ __('Your role no longer includes the accessibility consultant or community connector roles. Your public profile will be unpublished. However, if you edit your role to add the accessibility consultant or community connector roles again, you will be able to publish your profile again.') }}
-        </x-hearth-alert>
+        <div role="alert">
+            <x-hearth-alert type="warning" x-cloak x-show="(initialRoles.includes(2) || (initialRoles.includes(3)) && !roles.includes(2) && !roles.includes('2')) && !roles.includes(3) && !roles.includes('3')">
+                {{ __('Your role no longer includes the accessibility consultant or community connector roles. Your public profile will be unpublished. However, if you edit your role to add the accessibility consultant or community connector roles again, you will be able to publish your profile again.') }}
+            </x-hearth-alert>
+        </div>
 
         <p class="repel">
             <button type="button" class="secondary" x-on:click="history.back()">{{ __('Cancel') }}</button>
