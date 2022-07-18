0ed0fca7ecff4eceabc6e6552b93f4f60e21f263
Ned Zimmerman <ned@bight.dev>
2022-07-18 10:02:57 -0300

    test: add test

--- tests/Feature/UserSettingsTest.php
+++ tests/Feature/UserSettingsTest.php
@@ -4,6 +4,44 @@ use App\Models\PaymentType;
 use App\Models\User;
 use Database\Seeders\PaymentTypeSeeder;
 
+test('users can manage language preferences', function () {
+    $user = User::factory()->create(['context' => 'individual']);
+
+    $response = $this->actingAs($user)->get(localized_route('settings.edit-language-preferences'));
+
+    $response->assertOk();
+
+    $response = $this->actingAs($user)->put(localized_route('settings.update-language-preferences'), [
+        'locale' => 'en',
+        'signed_language' => 'ase',
+        'first_language' => 'ase',
+        'working_languages' => ['ase', 'en'],
+    ]);
+
+    $response->assertSessionHasNoErrors();
+    $response->assertRedirect(localized_route('settings.edit-language-preferences'));
+
+    expect($user->signed_language)->toEqual('ase');
+    expect($user->individual->first_language)->toEqual('ase');
+
+    $newUser = User::factory()->create(['context' => 'organization']);
+
+    $response = $this->actingAs($newUser)->get(localized_route('settings.edit-language-preferences'));
+
+    $response->assertOk();
+
+    $response = $this->actingAs($newUser)->put(localized_route('settings.update-language-preferences'), [
+        'locale' => 'fr',
+        'signed_language' => 'fcs',
+    ]);
+
+    $response->assertSessionHasNoErrors();
+    $response->assertRedirect(localized_route('settings.edit-language-preferences'));
+
+    expect($newUser->locale)->toEqual('fr');
+    expect($newUser->signed_language)->toEqual('fcs');
+});
+
 test('individual user can manage payment information settings', function () {
     $this->seed(PaymentTypeSeeder::class);
 
