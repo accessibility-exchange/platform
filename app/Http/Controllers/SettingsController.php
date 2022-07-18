137e0ab95fee3e6203deb7d9aa6f23793f20f111
Ned Zimmerman <ned@bight.dev>
2022-07-18 10:00:35 -0300

    feat: add language preferences (resolves #450)

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UpdateLanguagePreferencesRequest;
 use App\Http\Requests\UpdatePaymentInformationRequest;
 use App\Models\PaymentType;
 use Illuminate\Contracts\View\View;
@@ -12,6 +13,52 @@ use Spatie\LaravelOptions\Options;
 
 class SettingsController extends Controller
 {
+    public function editLanguagePreferences(): View
+    {
+        $user = Auth::user();
+        $individual = $user->individual;
+
+        $workingLanguages = [
+            $user->locale,
+        ];
+
+        if ($user->signed_language) {
+            $workingLanguages[] = $user->signed_language;
+        }
+
+        return view('settings.language-preferences', [
+            'user' => $user,
+            'individual' => $individual,
+            'languages' => Options::forArray(get_available_languages(true))->nullable(__('Choose a language…'))->toArray(),
+            'signedLanguages' => Options::forArray([
+                'ase' => __('American Sign Language (ASL)'),
+                'fcs' => 'Langue des signes québécoise (LSQ)',
+            ])->nullable(__('Choose a signed language…'))->toArray(),
+            'workingLanguages' => $workingLanguages,
+        ]);
+    }
+
+    public function updateLanguagePreferences(UpdateLanguagePreferencesRequest $request): RedirectResponse
+    {
+        $data = $request->validated();
+
+        $user = Auth::user();
+
+        $user->fill($data);
+
+        $user->save();
+
+        if ($individual = $user->individual) {
+            $individual->fill($data);
+
+            $individual->save();
+        }
+
+        flash(__('Your language preferences have been updated.'), 'success');
+
+        return redirect(localized_route('settings.edit-language-preferences'));
+    }
+
     public function editPaymentInformation(): View
     {
         Gate::allowIf(fn ($user) => $user->context === 'individual');
--- routes/settings.php
+++ routes/settings.php
@@ -6,6 +6,15 @@ Route::controller(SettingsController::class)
     ->prefix('settings')
     ->name('settings.')
     ->group(function () {
+        Route::multilingual('/language-preferences', 'editLanguagePreferences')
+            ->middleware(['auth'])
+            ->name('edit-language-preferences');
+
+        Route::multilingual('/language-preferences', 'updateLanguagePreferences')
+            ->method('put')
+            ->middleware(['auth'])
+            ->name('update-language-preferences');
+
         Route::multilingual('/payment-information', 'editPaymentInformation')
             ->middleware(['auth'])
             ->name('edit-payment-information');
