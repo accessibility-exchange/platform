dfac440045c3111cdef97be0d52b8be91e192c7e
Ned Zimmerman <ned@bight.dev>
2022-07-28 15:00:04 -0300

    feat: consolidate regulated organizations

--- database/migrations/2021_03_17_194241_create_regulated_organizations_table.php
+++ database/migrations/2021_03_17_194241_create_regulated_organizations_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateRegulatedOrganizationsTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -15,11 +15,26 @@ class CreateRegulatedOrganizationsTable extends Migration
     {
         Schema::create('regulated_organizations', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('slug');
-            $table->string('locality');
-            $table->string('region');
             $table->timestamps();
+            $table->timestamp('published_at')->nullable();
+            $table->json('name');
+            $table->json('slug');
+            $table->string('type');
+            $table->json('languages')->nullable();
+            $table->string('region')->nullable();
+            $table->string('locality')->nullable();
+            $table->json('about')->nullable();
+            $table->json('service_areas')->nullable();
+            $table->json('accessibility_and_inclusion_links')->nullable();
+            $table->json('social_links')->nullable();
+            $table->string('website_link')->nullable();
+            $table->string('contact_person_name')->nullable();
+            $table->string('contact_person_email')->nullable();
+            $table->string('contact_person_phone')->nullable();
+            $table->boolean('contact_person_vrs')->nullable();
+            $table->string('preferred_contact_method')->default('email');
+            $table->string('preferred_notification_method')->default('email');
+            $table->schemalessAttributes('notification_settings')->nullable();
         });
     }
 
@@ -32,4 +47,4 @@ class CreateRegulatedOrganizationsTable extends Migration
     {
         Schema::dropIfExists('regulated_organizations');
     }
-}
+};
