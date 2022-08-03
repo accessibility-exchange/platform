23ab5fbdd6c2c2170ad18ac648894b8d5a6c6c1f
Jure Ursic <ursic.jure@gmail.com>
2022-08-03 20:53:00 +0200

    Staging (#637)
    * fix: colab kube entrypoint additions
    
    * fix: colab kube artisan migrate --force
    
    Co-authored-by: Jure Ursic <jure@colab.coop>

--- .deploy/accessibility-app/entrypoint.sh
+++ .deploy/accessibility-app/entrypoint.sh
@@ -8,17 +8,24 @@ rsync -a /app/storage/ $FILES_PATH/
 rm -rf /app/storage
 
 ln -s $FILES_PATH /app/storage
+chown www-data:root /opt/data/storage/ -R
 
 if [ ! -f $FILES_PATH/../deploy.lock ]
 then
 
   touch $FILES_PATH/../deploy.lock
 
-  php artisan migrate --step
+  php artisan migrate --step --force
   php artisan google-fonts:fetch
   
 fi
 
 rm -rf $FILES_PATH/../deploy.lock
+
+php artisan storage:link
+php artisan cache:clear
+php artisan view:clear
+php artisan route:cache
+php artisan config:cache
 
 /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
