3b6468ff76e127284a463830507a87457aff1c1e
Marvin Roman <marvin@colab.coop>
2022-08-03 14:25:37 -0700

    ci: moved all artisan commands within the deploy check to make sure they run on a single container at a time

--- .deploy/accessibility-app/entrypoint.sh
+++ .deploy/accessibility-app/entrypoint.sh
@@ -15,24 +15,23 @@ then
 
   touch $FILES_PATH/../deploy.lock
 
-if [[ "$APP_ENV" == "production" ]]
-then
-  php artisan migrate --step
-else
-  php artisan migrate:fresh --step 
-  php artisan db:seed DevSeeder
-fi
+  if [[ "$APP_ENV" == "production" ]]
+  then
+    php artisan migrate --step
+  else
+    php artisan migrate:fresh --step 
+    php artisan db:seed DevSeeder
+  fi
 
-php artisan google-fonts:fetch
+  php artisan google-fonts:fetch
+  php artisan storage:link
+  php artisan cache:clear
+  php artisan view:clear
+  php artisan route:cache
+  php artisan config:cache
   
 fi
 
 rm -rf $FILES_PATH/../deploy.lock
-
-php artisan storage:link
-php artisan cache:clear
-php artisan view:clear
-php artisan route:cache
-php artisan config:cache
 
 /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
