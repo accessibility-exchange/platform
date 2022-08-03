2fe60591e2d5c5f577d8e2fc43d29668237224a3
Jure Ursic <jure@colab.coop>
2022-08-03 20:46:16 +0200

    fix: colab kube artisan migrate --force

--- .deploy/accessibility-app/entrypoint.sh
+++ .deploy/accessibility-app/entrypoint.sh
@@ -8,13 +8,14 @@ rsync -a /app/storage/ $FILES_PATH/
 rm -rf /app/storage
 
 ln -s $FILES_PATH /app/storage
+chown www-data:root /opt/data/storage/ -R
 
 if [ ! -f $FILES_PATH/../deploy.lock ]
 then
 
   touch $FILES_PATH/../deploy.lock
 
-  php artisan migrate --step
+  php artisan migrate --step --force
   php artisan google-fonts:fetch
   
 fi
