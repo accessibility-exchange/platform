b74eaadf5790dc66ba275f4e626443716e9d38f6
Ned Zimmerman <ned@bight.dev>
2022-08-03 18:38:47 -0300

    fix: resolve various signup issues (#636)
    fix: resolve 500 internal server error when creating Regulated Organization (resolves #631)
    fix: resolve 500 internal server error when saving Organization (fix #633)
    fix: prevent users signing up more than once with the same email (fix #632)

--- resources/views/organizations/edit/2.blade.php
+++ resources/views/organizations/edit/2.blade.php
@@ -92,7 +92,7 @@
                 </div>
             </fieldset>
 
-            <fieldset class="field @error('ethnoracial_identities') field--error @enderror" x-data="{hasEthnoracialIdentities: '{{ old('has_ethnoracial_identities', $organization->extra_attributes->get('has_ethnoracial_identities', '')) }}', otherEthnoracialIdentity: {{ old('other_ethnoracial_identity', !is_null($organization->other_ethnoracial_identity) && $organization->other_ethnoracial_identity !== '' ? 'true' : 'false') }}}">
+            <fieldset class="field @error('ethnoracial_identities') field--error @enderror" x-data="{hasEthnoracialIdentities: '{{ old('has_ethnoracial_identities', $organization->extra_attributes->get('has_ethnoracial_identities', '')) }}', otherEthnoracialIdentity: {{ old('other_ethnoracial_identity', $organization->other_ethnoracial_identity) ? 'true' : 'false' }}}">
                 <legend>{{ __('Does your organization :represent_or_serve_and_support a specific ethnoracial identity or identities? (required)', ['represent_or_serve_and_support' => ($organization->type === 'representative') ? __('represent') : __('serve and support')]) }}</legend>
                 <div class="field">
                     <input type="radio" name="has_ethnoracial_identities" id="has_ethnoracial_identities-1" value="1" @checked(old('has_ethnoracial_identities', $organization->extra_attributes->get('has_ethnoracial_identities', ''))) x-model="hasEthnoracialIdentities" /> <label for="has_ethnoracial_identities-1">{{ __('Yes') }}</label>
