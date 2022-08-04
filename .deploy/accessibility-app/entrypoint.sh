ef4a84a5029bfe7edddcf17767705cfbfb340458
Marvin Roman <marvin@colab.coop>
2022-08-04 15:31:27 -0700

    ci: changed caching declaration (#662) (#686)

--- .deploy/accessibility-app/entrypoint.sh
+++ .deploy/accessibility-app/entrypoint.sh
@@ -27,8 +27,8 @@ then
   php artisan storage:link
   php artisan cache:clear
   php artisan view:clear
-  php artisan route:cache
-  php artisan config:cache
+  php artisan optimize 
+  php artisan event:cache
   
 fi
 
