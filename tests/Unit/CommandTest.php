de6836310fcb3389b19252cfc87d27fdb36ae783
Marvin Roman <marvin@colab.coop>
2022-08-05 11:36:01 -0700

    test: removed command tests

--- tests/Unit/CommandTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-test('database refresh command', function () {
-    $this->artisan('db:refresh')->assertSuccessful();
-});
-
-test('initial deployment command', function () {
-    $this->artisan('deploy:initial')->assertSuccessful();
-});
