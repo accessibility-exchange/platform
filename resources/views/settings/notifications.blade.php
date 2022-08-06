000037842594f2fc8c422b0b7b0e532b057c4336
Ned Zimmerman <ned@bight.dev>
2022-08-05 22:46:05 -0300

    fix: success message in notification settings is cut off (fix #664)

--- resources/views/settings/notifications.blade.php
+++ resources/views/settings/notifications.blade.php
@@ -1,28 +1,24 @@
-<x-app-wide-layout>
+<x-app-wide-tabbed-layout>
     <x-slot name="title">{{ __('Notifications') }}</x-slot>
     <x-slot name="header">
-        <div class="full bg-white -mt-12 py-12 border-b-grey-3 border-solid border-b border-x-0 border-t-0">
-            <div class="center center:wide">
-                <ol class="breadcrumbs" role="list">
-                    <li><a href="{{ localized_route('welcome') }}">{{ __('Home') }}</a></li>
-                    <li><a href="{{ localized_route('settings.show') }}">{{ __('Settings') }}</a></li>
-                </ol>
-                <h1 id="notifications">
-                    {{ __('Notifications') }}
-                </h1>
-            </div>
-        </div>
+        <ol class="breadcrumbs" role="list">
+            <li><a href="{{ localized_route('welcome') }}">{{ __('Home') }}</a></li>
+            <li><a href="{{ localized_route('settings.show') }}">{{ __('Settings') }}</a></li>
+        </ol>
+        <h1 id="notifications">
+            {{ __('Notifications') }}
+        </h1>
     </x-slot>
 
     @if($user->context === 'individual')
-    <nav aria-labelledby="notifications" class="full bg-white">
+    <nav aria-labelledby="notifications" class="full bg-white shadow-md">
         <div class="center center:wide">
             <ul role="list" class="flex gap-6 -mt-4">
                 <li class="w-1/2">
-                    <x-nav-link class="inline-flex items-center justify-center w-full" :href="localized_route('settings.edit-notification-preferences')" :active="request()->localizedRouteIs('settings.edit-notification-preferences')">{{ __('Manage notifications') }}</x-nav-link>
+                    <x-nav-link class="inline-flex items-center justify-center w-full border-t-0" :href="localized_route('settings.edit-notification-preferences')" :active="request()->localizedRouteIs('settings.edit-notification-preferences')">{{ __('Manage notifications') }}</x-nav-link>
                 </li>
                 <li class="w-1/2">
-                    <x-nav-link class="inline-flex items-center justify-center w-full" :href="localized_route('notification-list.show')" :active="request()->localizedRouteIs('notification-list.show')">{{ __('Notification list') }}</x-nav-link>
+                    <x-nav-link class="inline-flex items-center justify-center w-full border-t-0" :href="localized_route('notification-list.show')" :active="request()->localizedRouteIs('notification-list.show')">{{ __('Notification list') }}</x-nav-link>
                 </li>
             </ul>
         </div>
@@ -36,4 +32,4 @@
     <p>{{ __('The Accessibility Exchange will occasionally send you notifications, based on what you chose to be notified of here.') }}</p>
 
     @include('settings.notifications.'.$user->context)
-</x-app-wide-layout>
+</x-app-wide-tabbed-layout>
