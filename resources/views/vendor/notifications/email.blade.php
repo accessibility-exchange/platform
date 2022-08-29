4f8789b169da9fc52c7941e3b64d0eb8fb9dc901
Ned Zimmerman <ned@bight.dev>
2022-08-29 12:48:49 -0300

    fix: restore email templates, exclude vendor views from Prettier (#961)
    This partially reverts commit d31e9fbb14068a3ee28d0850104ffb08661d970f.

--- resources/views/vendor/notifications/email.blade.php
+++ resources/views/vendor/notifications/email.blade.php
@@ -1,56 +1,61 @@
 @component('mail::message')
-    {{-- Greeting --}}
-    @if (!empty($greeting))
-        # {{ $greeting }}
-    @else
-        @if ($level === 'error')
-            # @lang('mail.error')
-        @else
-            # @lang('mail.greeting')
-        @endif
-    @endif
+{{-- Greeting --}}
+@if (! empty($greeting))
+# {{ $greeting }}
+@else
+@if ($level === 'error')
+# @lang('mail.error')
+@else
+# @lang('mail.greeting')
+@endif
+@endif
 
-    {{-- Intro Lines --}}
-    @foreach ($introLines as $line)
-        {{ $line }}
-    @endforeach
+{{-- Intro Lines --}}
+@foreach ($introLines as $line)
+{{ $line }}
 
-    {{-- Action Button --}}
-    @isset($actionText)
-        <?php
-        switch ($level) {
-            case 'success':
-            case 'error':
-                $color = $level;
-                break;
-            default:
-                $color = 'primary';
-        }
-        ?>
-        @component('mail::button', ['url' => $actionUrl, 'color' => $color])
-            {{ $actionText }}
-        @endcomponent
-    @endisset
+@endforeach
 
-    {{-- Outro Lines --}}
-    @foreach ($outroLines as $line)
-        {{ $line }}
-    @endforeach
+{{-- Action Button --}}
+@isset($actionText)
+<?php
+    switch ($level) {
+        case 'success':
+        case 'error':
+            $color = $level;
+            break;
+        default:
+            $color = 'primary';
+    }
+?>
+@component('mail::button', ['url' => $actionUrl, 'color' => $color])
+{{ $actionText }}
+@endcomponent
+@endisset
 
-    {{-- Salutation --}}
-    @if (!empty($salutation))
-        {{ $salutation }}
-    @else
-        @lang('mail.salutation'),<br>
-        {{ config('app.name') }}
-    @endif
+{{-- Outro Lines --}}
+@foreach ($outroLines as $line)
+{{ $line }}
 
-    {{-- Subcopy --}}
-    @isset($actionText)
-        @slot('subcopy')
-            @lang('mail.link_guidance', [
-                'actionText' => $actionText,
-            ]) <span class="break-all">[{{ $displayableActionUrl }}]({{ $actionUrl }})</span>
-        @endslot
-    @endisset
+@endforeach
+
+{{-- Salutation --}}
+@if (! empty($salutation))
+{{ $salutation }}
+@else
+@lang('mail.salutation'),<br>
+{{ config('app.name') }}
+@endif
+
+{{-- Subcopy --}}
+@isset($actionText)
+@slot('subcopy')
+@lang(
+    'mail.link_guidance',
+    [
+        'actionText' => $actionText,
+    ]
+) <span class="break-all">[{{ $displayableActionUrl }}]({{ $actionUrl }})</span>
+@endslot
+@endisset
 @endcomponent
