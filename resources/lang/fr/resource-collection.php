84f4fda08f9d3aa5dd7ce0fc290492ca371cf90d
Ned Zimmerman <ned@bight.dev>
2022-09-06 16:22:48 -0300

    chore: upgrade to Hearth v3, integrate changes (#1038)

---
+++ resources/lang/fr/resource-collection.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'create_title' => 'Créer une collection de ressources',
+    'label_title' => 'Titre de la collection de ressources',
+    'label_language' => 'Langue de la collection de ressources',
+    'label_description' => 'Description de la collection de ressources',
+    'action_create' => 'Créer la collection de ressources',
+    'edit_title' => 'Modifier la collection de ressources',
+    'edit_resource_collection' => 'Éditer la collection de ressources',
+    'action_delete' => 'Supprimer la collection de ressources',
+    'delete_title' => 'Supprimer la collection de ressources',
+    'delete_intro' => 'Votre collection de ressources sera supprimée et ne pourra pas être récupérée. Si vous voulez toujours supprimer votre collection, veuillez entrer votre mot de passe actuel pour continuer.',
+    'create_succeeded' => 'Votre collection de ressources a été créée.',
+    'update_succeeded' => 'Votre collection de ressources a été mise à jour.',
+    'destroy_succeeded' => 'Votre collection de ressources a été supprimée.',
+    'index_title' => 'Resource collections',
+    'none_found' => 'No resource collections found.',
+];
