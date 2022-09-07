51ff641bfaaca16bf988bdd79b2946100f8f14d3
Giovanni Tirloni <giovanni.tirloni@gmail.com>
2022-09-07 18:04:43 -0300

    fix: run deployment steps as sail user

---
+++ bin/refresh-dev.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -ex
+
+CONTAINER_USER="sail"
+COMPOSE_CMD="/usr/local/bin/docker-compose -f docker-compose.cloud.yml"
+EXEC_CMD="$COMPOSE_CMD exec -T --user $CONTAINER_USER app"
+
+$EXEC_CMD ./artisan migrate:fresh --force
+$EXEC_CMD ./artisan db:seed DevSeeder --force
