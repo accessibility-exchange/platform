7cc98b491388c8b2965759a5f71261c47245dafb
Ned Zimmerman <ned@bight.dev>
2022-09-18 22:30:15 -0300

    test: scaffold tests

---
+++ tests/Feature/Livewire/EstimatesAndEngagementsTest.php
@@ -0,0 +1,13 @@
+<?php
+
+test('only administrators can access estimates and engagements page', function () {
+    $this->markTestIncomplete();
+});
+
+test('estimate can be marked as returned', function () {
+    $this->markTestIncomplete();
+});
+
+test('agreement can be marked as received', function () {
+    $this->markTestIncomplete();
+});
