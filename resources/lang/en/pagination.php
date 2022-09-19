097bb3476155c87ecf2299d6c2d245072025d5f8
Ned Zimmerman <ned@bight.dev>
2022-09-18 22:27:03 -0300

    feat: implement pagination component (resolves #361)

--- resources/lang/en/pagination.php
+++ resources/lang/en/pagination.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'previous' => 'Previous',
-    'next' => 'Next',
+    'previous' => 'Previous page',
+    'next' => 'Next page',
 
 ];
--- resources/views/vendor/livewire/tailwind.blade.php
+++ resources/views/vendor/livewire/tailwind.blade.php
@@ -2,132 +2,95 @@
     @if ($paginator->hasPages())
         @php(isset($this->numberOfPaginatorsRendered[$paginator->getPageName()]) ? $this->numberOfPaginatorsRendered[$paginator->getPageName()]++ : ($this->numberOfPaginatorsRendered[$paginator->getPageName()] = 1))
 
-        <nav class="flex items-center justify-between" role="navigation" aria-label="Pagination Navigation">
-            {{-- <div class="flex flex-1 justify-between sm:hidden"> --}}
-            {{-- <span> --}}
-            {{-- @if ($paginator->onFirstPage()) --}}
-            {{-- <span class="inline-flex rounded-full px-0 py-0"> --}}
-            {{-- <x-heroicon-s-chevron-left class="text-graphite-7" /> --}}
-            {{-- <span class="sr-only">{{ __('pagination.previous') }}</span> --}}
-            {{-- </span> --}}
-            {{-- @else --}}
-            {{-- <button class="borderless inline-flex rounded-full px-0 py-0" --}}
-            {{-- wire:click="previousPage('{{ $paginator->getPageName() }}')" wire:loading.attr="disabled" --}}
-            {{-- dusk="previousPage{{ $paginator->getPageName() == 'page' ? '' : '.' . $paginator->getPageName() }}.before"> --}}
-            {{-- <x-heroicon-s-chevron-left class="text-graphite-7" /> --}}
-            {{-- <span class="sr-only">{{ __('pagination.previous') }}</span> --}}
-            {{-- </button> --}}
-            {{-- @endif --}}
-            {{-- </span> --}}
+        <nav class="flex flex-col items-center justify-between" role="navigation" aria-label="Pagination Navigation">
+            <div class="w-full text-center" role="alert" aria-live="polite">
+                <p>
+                    {{ __('Showing :current_start to :current_end of :total results', [
+                        'current_start' => $paginator->firstItem(),
+                        'current_end' => $paginator->lastItem(),
+                        'total' => $paginator->total(),
+                    ]) }}
+                </p>
+            </div>
 
-            {{-- <span> --}}
-            {{-- @if ($paginator->hasMorePages()) --}}
-            {{-- <button class="borderless inline-flex rounded-full px-0 py-0" --}}
-            {{-- wire:click="nextPage('{{ $paginator->getPageName() }}')" wire:loading.attr="disabled" --}}
-            {{-- dusk="nextPage{{ $paginator->getPageName() == 'page' ? '' : '.' . $paginator->getPageName() }}.before"> --}}
-            {{-- <x-heroicon-s-chevron-right class="text-graphite-7" /> --}}
-            {{-- <span class="sr-only">{{ __('pagination.next') }}</span> --}}
-            {{-- </button> --}}
-            {{-- @else --}}
-            {{-- <span class="inline-flex rounded-full px-0 py-0"> --}}
-            {{-- <x-heroicon-s-chevron-right class="text-graphite-7" /> --}}
-            {{-- <span class="sr-only">{{ __('pagination.next') }}</span> --}}
-            {{-- </span> --}}
-            {{-- @endif --}}
-            {{-- </span> --}}
-            {{-- </div> --}}
+            <div class="flex w-full justify-between sm:hidden">
+                <ul class="pagination flex w-full flex-row items-center justify-between" role="list">
+                    <li>
+                        @if ($paginator->onFirstPage())
+                            <span class="spacer"></span>
+                        @else
+                            <a href="{{ route(RalphJSmit\Livewire\Urls\Facades\Url::currentRoute(), ['page' => $paginator->currentPage() - 1]) }}"
+                                aria-label="{{ __('pagination.previous') }}"
+                                wire:click.prevent="previousPage('{{ $paginator->getPageName() }}')"
+                                wire:loading.attr="disabled" rel="prev">
+                                <x-heroicon-s-chevron-left class="h-5 w-5" aria-hidden="true" />
+                            </a>
+                        @endif
+                    </li>
+
+                    <li>
+                        @if ($paginator->hasMorePages())
+                            <a href="{{ route(RalphJSmit\Livewire\Urls\Facades\Url::currentRoute(), ['page' => $paginator->currentPage() + 1]) }}"
+                                aria-label="{{ __('pagination.next') }}"
+                                wire:click.prevent="nextPage('{{ $paginator->getPageName() }}')"
+                                wire:loading.attr="disabled" rel="prev">
+                                <x-heroicon-s-chevron-right class="h-5 w-5" aria-hidden="true" />
+                            </a>
+                        @else
+                            <span class="spacer"></span>
+                        @endif
+                    </li>
+                </ul>
+            </div>
 
             <div class="hidden w-full space-y-6 sm:flex sm:flex-col sm:items-center sm:justify-between">
-                <div class="w-full text-center">
-                    <p class="">
-                        {{ __('Showing :current_start to :current_end of :total results', [
-                            'current_start' => $paginator->firstItem(),
-                            'current_end' => $paginator->lastItem(),
-                            'total' => $paginator->total(),
-                        ]) }}
-                    </p>
+                <ul class="pagination flex flex-row items-center justify-center gap-2" role="list">
+                    @if (!$paginator->onFirstPage())
+                        {{-- Previous Page Link --}}
+                        <li>
+                            <a href="{{ route(RalphJSmit\Livewire\Urls\Facades\Url::currentRoute(), ['page' => $paginator->currentPage() - 1]) }}"
+                                aria-label="{{ __('pagination.previous') }}"
+                                wire:click.prevent="previousPage('{{ $paginator->getPageName() }}')"
+                                wire:loading.attr="disabled" rel="prev">
+                                <x-heroicon-s-chevron-left class="h-5 w-5" aria-hidden="true" />
+                            </a>
+                        </li>
+                    @endif
 
-                </div>
+                    {{-- Pagination Elements --}}
+                    @foreach ($elements as $element)
+                        {{-- "Three Dots" Separator --}}
+                        @if (is_string($element))
+                            <li><span class="spacer">{{ $element }}</span></li>
+                        @endif
 
-                <div>
-                    <span class="relative z-0 inline-flex rounded-md shadow-sm">
-                        <span>
-                            {{-- Previous Page Link --}}
-                            @if ($paginator->onFirstPage())
-                                <span aria-disabled="true" aria-label="{{ __('pagination.previous') }}">
-                                    <span aria-hidden="true">
-                                        <svg class="h-5 w-5" fill="currentColor" viewBox="0 0 20 20">
-                                            <path fill-rule="evenodd"
-                                                d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-                                                clip-rule="evenodd" />
-                                        </svg>
-                                    </span>
-                                </span>
-                            @else
-                                <button aria-label="{{ __('pagination.previous') }}"
-                                    wire:click="previousPage('{{ $paginator->getPageName() }}')"
-                                    dusk="previousPage{{ $paginator->getPageName() == 'page' ? '' : '.' . $paginator->getPageName() }}.after"
-                                    rel="prev">
-                                    <x-heroicon-s-chevron-left class="text-graphite-7" />
-                                </button>
-                            @endif
-                        </span>
+                        {{-- Array Of Links --}}
+                        @if (is_array($element))
+                            @foreach ($element as $page => $url)
+                                <li
+                                    wire:key="paginator-{{ $paginator->getPageName() }}-{{ $this->numberOfPaginatorsRendered[$paginator->getPageName()] }}-page{{ $page }}">
+                                    <a href="{{ route(RalphJSmit\Livewire\Urls\Facades\Url::currentRoute(), ['page' => $page]) }}"
+                                        wire:click.prevent="gotoPage({{ $page }}, '{{ $paginator->getPageName() }}')"
+                                        @if ($page == $paginator->currentPage()) aria-current="page" @endif>
+                                        <span class="visually-hidden">{{ __('page') }}</span> {{ $page }}
+                                    </a>
+                                </li>
+                            @endforeach
+                        @endif
+                    @endforeach
 
-                        {{-- Pagination Elements --}}
-                        @foreach ($elements as $element)
-                            {{-- "Three Dots" Separator --}}
-                            @if (is_string($element))
-                                <span class="px-4">{{ $element }}</span>
-                            @endif
-
-                            {{-- Array Of Links --}}
-                            @if (is_array($element))
-                                @foreach ($element as $page => $url)
-                                    <span
-                                        wire:key="paginator-{{ $paginator->getPageName() }}-{{ $this->numberOfPaginatorsRendered[$paginator->getPageName()] }}-page{{ $page }}">
-                                        @if ($page == $paginator->currentPage())
-                                            <span aria-current="page">
-                                                <span>{{ $page }}</span>
-                                            </span>
-                                        @else
-                                            <button class="rounded-full underline"
-                                                aria-label="{{ __('Go to page :page', ['page' => $page]) }}"
-                                                wire:click="gotoPage({{ $page }}, '{{ $paginator->getPageName() }}')">
-                                                {{ $page }}
-                                            </button>
-                                        @endif
-                                    </span>
-                                @endforeach
-                            @endif
-                        @endforeach
-
-                        <span>
-                            {{-- Next Page Link --}}
-                            @if ($paginator->hasMorePages())
-                                <button aria-label="{{ __('pagination.next') }}"
-                                    wire:click="nextPage('{{ $paginator->getPageName() }}')"
-                                    dusk="nextPage{{ $paginator->getPageName() == 'page' ? '' : '.' . $paginator->getPageName() }}.after"
-                                    rel="next">
-                                    <svg class="h-5 w-5" fill="currentColor" viewBox="0 0 20 20">
-                                        <path fill-rule="evenodd"
-                                            d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                                            clip-rule="evenodd" />
-                                    </svg>
-                                </button>
-                            @else
-                                <span aria-disabled="true" aria-label="{{ __('pagination.next') }}">
-                                    <span aria-hidden="true">
-                                        <svg class="h-5 w-5" fill="currentColor" viewBox="0 0 20 20">
-                                            <path fill-rule="evenodd"
-                                                d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                                                clip-rule="evenodd" />
-                                        </svg>
-                                    </span>
-                                </span>
-                            @endif
-                        </span>
-                    </span>
-                </div>
+                    @if ($paginator->hasMorePages())
+                        {{-- Next Page Link --}}
+                        <li>
+                            <a href="{{ route(RalphJSmit\Livewire\Urls\Facades\Url::currentRoute(), ['page' => $paginator->currentPage() + 1]) }}"
+                                aria-label="{{ __('pagination.next') }}"
+                                wire:click.prevent="nextPage('{{ $paginator->getPageName() }}')"
+                                wire:loading.attr="disabled" rel="prev">
+                                <x-heroicon-s-chevron-right class="h-5 w-5" aria-hidden="true" />
+                            </a>
+                        </li>
+                    @endif
+                </ul>
             </div>
         </nav>
     @endif
