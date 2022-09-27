edda2e871ed82e2c1820ff4cba6ab433b4339c75
Ned Zimmerman <ned@bight.dev>
2022-09-27 10:25:33 -0300

    chore(localization): update translations (#1093)
    * chore(localization): translate auth.php into French, Canada
    
    * chore(localization): translate consulting-services.php into French, Canada

--- resources/lang/fr/consulting-services.php
+++ resources/lang/fr/consulting-services.php
@@ -2,7 +2,7 @@
 
 return [
     'booking-providers' => 'Booking accessibility service providers',
-    'planning-consultation' => 'Planning the consultation',
+    'planning-consultation' => 'Planification de la consultation',
     'running-consultation' => 'Running the consultation',
     'analysis' => 'Analysis of collected information',
     'writing-reports' => 'Writing accessibility reports',
