f3d10b85bd598f397b9d71eaec39ca01bbb8f112
Ned Zimmerman <ned@bight.dev>
2022-10-03 16:38:08 -0300

    fix: clicking outside should always close modals (resolve #1067) (#1135)
    fix: clicking outside should always close modals

--- resources/views/components/translation-manager.blade.php
+++ resources/views/components/translation-manager.blade.php
@@ -7,26 +7,27 @@
                 @endif
             </p>
             <template x-teleport="body">
-                <div class="modal-wrapper" x-show="showingModal">
-                    <form class="modal stack" action="{{ localized_route('translations.destroy') }}" method="post"
-                        @keydown.escape.window="hideModal">
-                        <h3>{{ __('Remove language') }}</h3>
+                <div class="modal-wrapper" x-show="showingModal" @keydown.escape.window="hideModal">
+                    <div class="modal stack" @click.outside="hideModal">
+                        <form class="stack" action="{{ localized_route('translations.destroy') }}" method="post">
+                            <h3>{{ __('Remove language') }}</h3>
 
-                        <p>{{ __('Are you sure you want to remove :language? Any translations that you’ve entered will be lost.', ['language' => get_language_exonym($language)]) }}
-                        </p>
+                            <p>{{ __('Are you sure you want to remove :language? Any translations that you’ve entered will be lost.', ['language' => get_language_exonym($language)]) }}
+                            </p>
 
-                        <p class="repel">
-                            <button type="button" @click="hideModal">{{ __('Cancel') }}</button>
-                            <button class="secondary">{{ __('Remove') }}</button>
-                        </p>
+                            <p class="repel">
+                                <button type="button" @click="hideModal">{{ __('Cancel') }}</button>
+                                <button class="secondary">{{ __('Remove') }}</button>
+                            </p>
 
-                        <x-hearth-input name="translatable_type" type="hidden" :value="get_class($model)" />
-                        <x-hearth-input name="translatable_id" type="hidden" :value="$model->id" />
-                        <x-hearth-input name="language" type="hidden" :value="$language" />
+                            <x-hearth-input name="translatable_type" type="hidden" :value="get_class($model)" />
+                            <x-hearth-input name="translatable_id" type="hidden" :value="$model->id" />
+                            <x-hearth-input name="language" type="hidden" :value="$language" />
 
-                        @csrf
-                        @method('put')
-                    </form>
+                            @csrf
+                            @method('put')
+                        </form>
+                    </div>
                 </div>
             </template>
         </div>
@@ -35,28 +36,30 @@
     <div x-data="modal()">
         <button class="secondary" @click="showModal">{{ __('Add language') }}</button>
         <template x-teleport="body">
-            <div class="modal-wrapper" x-show="showingModal">
-                <form class="modal stack" action="{{ localized_route('translations.add') }}" method="post"
-                    @keydown.escape.window="hideModal">
-                    <h3>{{ __('Add language') }}</h3>
+            <div class="modal-wrapper" x-show="showingModal" @keydown.escape.window="hideModal">
+                <div class="modal stack" @click.outside="hideModal">
+                    <form class="stack" action="{{ localized_route('translations.add') }}" method="post"
+                        @keydown.escape.window="hideModal">
+                        <h3>{{ __('Add language') }}</h3>
 
-                    <div class="field @error('new_language') field--error @enderror">
-                        <x-hearth-label for="new_language">{{ __('Language') }}</x-hearth-label>
-                        <x-hearth-select name="new_language" x-data="autocomplete()" :options="$availableLanguages" :selected="old('new_language', '')"
-                            required />
-                    </div>
+                        <div class="field @error('new_language') field--error @enderror">
+                            <x-hearth-label for="new_language">{{ __('Language') }}</x-hearth-label>
+                            <x-hearth-select name="new_language" x-data="autocomplete()" :options="$availableLanguages"
+                                :selected="old('new_language', '')" required />
+                        </div>
 
-                    <p class="repel">
-                        <button class="secondary" type="button" @click="hideModal">{{ __('Cancel') }}</button>
-                        <button>{{ __('Add language') }}</button>
-                    </p>
+                        <p class="repel">
+                            <button class="secondary" type="button" @click="hideModal">{{ __('Cancel') }}</button>
+                            <button>{{ __('Add language') }}</button>
+                        </p>
 
-                    <x-hearth-input name="translatable_type" type="hidden" :value="get_class($model)" />
-                    <x-hearth-input name="translatable_id" type="hidden" :value="$model->id" />
+                        <x-hearth-input name="translatable_type" type="hidden" :value="get_class($model)" />
+                        <x-hearth-input name="translatable_id" type="hidden" :value="$model->id" />
 
-                    @csrf
-                    @method('put')
-                </form>
+                        @csrf
+                        @method('put')
+                    </form>
+                </div>
             </div>
         </template>
     </div>
