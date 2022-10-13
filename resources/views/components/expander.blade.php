5a57d07ee36602d521608aa5df1a59bc1deef9a9
Ned Zimmerman <ned@bight.dev>
2022-10-13 10:37:38 -0300

    fix: resolve conflict between Livewire and the expander component (resolves #1149) (#1170)

--- resources/views/components/expander.blade.php
+++ resources/views/components/expander.blade.php
@@ -1,15 +1,11 @@
-<div class="stack" x-data="{ expanded: false, initialized: false }" x-init="$refs.expander.classList.add('expander');
-initialized = true;" x-ref="expander">
+<div class="expander stack" x-data="{ expanded: false }">
     <x-heading class="title" :level="$level">
-        <span x-show="!initialized">{{ $summary }}</span>
-        <template x-if="initialized">
-            <button type="button" x-bind:aria-expanded="expanded.toString()" x-on:click="expanded = !expanded">
-                {{ $summary }}
-                <x-heroicon-o-chevron-down class="indicator" aria-hidden="true" />
-            </button>
-        </template>
+        <button type="button" x-bind:aria-expanded="expanded.toString()" x-on:click="expanded = !expanded">
+            {{ $summary }}
+            <x-heroicon-o-chevron-down class="indicator" aria-hidden="true" />
+        </button>
     </x-heading>
-    <div class="stack" x-show="initialized ? expanded : true">
+    <div class="stack" x-show="expanded">
         {!! $slot ?? '' !!}
     </div>
 </div>
