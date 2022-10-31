5a1e6ee98515c1b03bbe0d0f805ffa3dc052c255
Ned Zimmerman <ned@bight.dev>
2022-10-31 10:22:53 -0300

    fix: show icon for password input (resolves #943)

--- resources/views/components/password-input.blade.php
+++ resources/views/components/password-input.blade.php
@@ -6,6 +6,7 @@
         {{ $required ? 'required' : '' }} @disabled($disabled) {!! $describedBy() ? 'aria-describedby="' . $describedBy() . '"' : '' !!} {!! $invalid ? 'aria-invalid="true"' : '' !!}>
     <div class="field">
         <x-hearth-checkbox :disabled="$disabled" :name="'show_' . $name" x-model="show" />
-        <x-hearth-label :for="'show_' . $name" :value="__('Show password')" />
+        <x-hearth-label class="inline-flex items-center" :for="'show_' . $name">{{ __('Show password') }} @svg('heroicon-o-eye', 'ml-2 icon--lg')
+        </x-hearth-label>
     </div>
 </div>
