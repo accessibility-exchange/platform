9f81186080760a90c7c7609cc6c210da8c2c29c9
Daniel Cho <chosww@gmail.com>
2022-11-02 15:32:43 -0400

    fix: update copies

--- app/Enums/UserContext.php
+++ app/Enums/UserContext.php
@@ -15,7 +15,7 @@ enum UserContext: string
         return [
             'individual' => __('Individual'),
             'organization' => __('Community Organization'),
-            'regulated-organization' => __('Regulated Organizations: Business, Federal Government and Public Sector Organizations'),
+            'regulated-organization' => __('Federally Regulated Organizations'),
             'employee' => __('Employee Seeking Training'),
         ];
     }
@@ -23,10 +23,10 @@ enum UserContext: string
     public function description(): string
     {
         return match ($this) {
-            self::Individual => __('I am someone who has lived experience of being disabled or Deaf, or I am a family member or supporter of a person who is disabled or Deaf.'),
-            self::Organization => __('I am a part of a community organization that represents or serves the disability community, the Deaf community. Or, I am a part of a civil society.'),
-            self::RegulatedOrganization => __('I work for a business, the federal government or a public sector organization regulated under the Accessible Canada Act.'),
-            self::Employee => __('I am an employee seeking training assigned by my organization or business.'),
+            self::Individual => __('I am a person with a disability, a Deaf person, or am a family member or supporter.'),
+            self::Organization => __('I am with a community organization that represents or serves the disability community, the Deaf community, or another kind of civil society organization that is concerned about accessibility issues.'),
+            self::RegulatedOrganization => __('I work for a private business, the federal government, or a public sector organization regulated under the Accessible Canada Act.'),
+            self::Employee => __('I am seeking training assigned by my organization or business.'),
             default => null,
         };
     }
