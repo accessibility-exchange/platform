7aced63680b113f3fb8110c58a44ece6eff447c6
Ned Zimmerman <ned@bight.dev>
2022-11-08 16:07:05 -0400

    test: increase coverage

---
+++ tests/Feature/Livewire/ThemeSwitcherTest.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Http\Livewire\ThemeSwitcher;
+use App\Models\User;
+
+test('setting theme updates current user preference', function () {
+    $user = User::factory()->create();
+    $this->actingAs($user);
+
+    $this->livewire(ThemeSwitcher::class)
+        ->call('setTheme', 'dark');
+
+    expect($user->fresh()->theme)->toEqual('dark');
+});
