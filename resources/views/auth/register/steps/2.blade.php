dec27636edc97bc39f3456dc17f06861920ea04b
Daniel Cho <chosww@gmail.com>
2022-11-09 16:52:41 -0500

    fix: update based on PR feedback

--- resources/views/auth/register/steps/2.blade.php
+++ resources/views/auth/register/steps/2.blade.php
@@ -1,6 +1,9 @@
 <form class="stack" method="POST" action="{{ localized_route('register-context') }}" novalidate>
     <fieldset class="field @error('context') field--error @enderror stack">
         <legend>{{ __('Tell us about who you are.') }}</legend>
+        <x-hearth-hint for="context">
+            {{ __('If more than one of these apply to you, you have the option to register additional accounts separately.') }}
+        </x-hearth-hint>
         <x-expander :summary="__('Learn more')" level="3">
             <p>
                 {{ __('You can only choose one of these options for each account. So, if you are a Deaf person who would like to participate in consultations, and you are also the contact for an organization that serves Deaf people, you should create two separate accounts, one as an individual, and as a Community Organization. That way, we can be sure to show you the right information based on who you’ve joined as.') }}
