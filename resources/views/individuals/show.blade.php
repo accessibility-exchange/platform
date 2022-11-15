1f827e783c4c81e97bb478f60f6f4bc6473e3e72
Jure Ursic <ursic.jure@gmail.com>
2022-11-15 17:18:51 +0100

    chore: integrate cron changes from staging (#1307)
    * ci(deploy): permissions fix after syncing files to storage (#1264)
    
    * fix: resolve invalid status check (fixes #1272) (#1273)
    
    * feat: cron schedule container support
    
    * feat: cron schedule container support
    
    * feat: cron schedule container support
    
    Co-authored-by: Marvin Roman <marvin@colab.coop>
    Co-authored-by: Ned Zimmerman <ned@bight.dev>
    Co-authored-by: Jure Ursic <jure@colab.coop>

--- resources/views/individuals/show.blade.php
+++ resources/views/individuals/show.blade.php
@@ -3,7 +3,7 @@
     <x-slot name="header">
         @if (auth()->hasUser() &&
             auth()->user()->isAdministrator() &&
-            $individual->checkStatus('suspended'))
+            $individual->user->checkStatus('suspended'))
             @push('banners')
                 <div class="banner banner--error">
                     <div class="center center:wide">
