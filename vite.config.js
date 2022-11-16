ddf99b161b551385c21c592fbedb8053b9f31731
Ned Zimmerman <ned@bight.dev>
2022-11-16 14:52:59 -0400

    feat: implement admin color theme [WIP]

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
+                "resources/css/filament.css",
                 "resources/css/app.css",
                 "resources/js/app.js"
             ],
