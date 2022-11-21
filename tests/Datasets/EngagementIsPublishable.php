9738a6fd7c23dfc07bdf0887aaaab3ce4c994864
Ned Zimmerman <ned@bight.dev>
2022-11-21 12:16:50 -0400

    fix: restore hard-coded config

--- tests/Datasets/EngagementIsPublishable.php
+++ tests/Datasets/EngagementIsPublishable.php
@@ -3,7 +3,7 @@
 dataset('engagementIsPublishable', function () {
     $baseModel = [
         'name' => ['en' => 'Workshop'],
-        'languages' => fn () => config('locales.supported'),
+        'languages' => ['en', 'fr', 'asl', 'sql'],
         'who' => 'individuals',
         'format' => 'workshop',
         'recruitment' => 'open-call',
