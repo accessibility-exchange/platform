5050bb59ac1f5b32c9cf8a0bf51e90f1804a819b
Ned Zimmerman <ned@bight.dev>
2022-11-21 08:53:58 -0400

    fix: don’t duplicate English files into ASL

--- resources/lang/asl/mail.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'error' => 'Sorry!',
-    'greeting' => 'Hello!',
-    'salutation' => 'Regards',
-    'link_guidance' => 'If you’re having trouble clicking the ":actionText" button, copy and paste the URL below into your web browser:',
-];
--- resources/lang/asl/roles.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'admin' => 'Administrator',
-    'manager' => 'Manager',
-    'member' => 'Member',
-];
