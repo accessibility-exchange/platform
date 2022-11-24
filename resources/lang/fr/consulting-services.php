df6efaec955b1cc152c7c63586c4932135793b43
Ned Zimmerman <ned@bight.dev>
2022-11-24 13:53:57 -0400

    chore(localization): translate consulting-services.php into French, Canada

--- resources/lang/fr/consulting-services.php
+++ resources/lang/fr/consulting-services.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    'booking-providers' => 'Réservation de prestataires de services en matière d\'accessibilité',
-    'planning-consultation' => 'Planification de la consultation',
-    'running-consultation' => 'Gestion de la consultation',
-    'analysis' => 'Analyse des informations recueillies',
-    'writing-reports' => 'Rédaction de rapports relatifs à l\'accessibilité',
+    'booking-providers' => __('Réservation de prestataires de services en matière d\'accessibilité'),
+    'planning-consultation' => __('Designing and organizing accessibility plans'),
+    'running-consultation' => __('Designing and organizing a consultation'),
+    'analysis' => __('Analyse des informations recueillies'),
+    'writing-reports' => __('Rédaction de rapports relatifs à l\'accessibilité'),
 ];
