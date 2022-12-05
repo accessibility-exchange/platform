4743d3b3cf84cc247c9d0f463bb42be0908b7118
Justin Obara <jobara@users.noreply.github.com>
2022-12-05 13:17:38 -0500

    feat: add basic text-to-speech (resolves #497) (#1376)
    * feat: add Infusion's Orator for TTS support
    
    * feat: style the TTS mark
    
    * fix: styling issues for tts mark in expander
    
    * refactor: reorganize tts templates
    
    * feat: style tts controller
    
    * fix: typo
    
    * feat: style the tts popup control
    
    * fix: multiple content containers
    
    * feat: theme selection highlight
    
    * feat: add arrow to controller button
    
    * feat: style tts popup
    
    * style: lint fixes
    
    * chore: update localizations

---
+++ resources/views/components/tts/popup.blade.php
@@ -0,0 +1,4 @@
+<button class="flc-orator-selectionReader-control" type="button">
+    @svg('tae-play', 'icon--xl tts-selection-play')
+    @svg('tae-stop', 'icon--xl tts-selection-stop')
+</button>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,6 +16,8 @@
                 @include('partials.flash-messages')
                 <!-- Page Heading -->
                 <header class="stack">
+                    <!-- Text to Speech -->
+                    <x-tts.orator />
                     {{ $header }}
                 </header>
                 <!-- Page Content -->
