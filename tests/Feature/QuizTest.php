2947a73ccd992e1bfcb5f5f457f267734de9ed07
Daniel Cho <chosww@gmail.com>
2022-12-07 16:10:24 -0500

    fix: increase coverage for QuizTest

--- tests/Feature/QuizTest.php
+++ tests/Feature/QuizTest.php
@@ -125,3 +125,70 @@ test('users can view quiz results on finishing it', function () {
     $this->assertNotNull(DB::table('course_user')->where([['course_id', $course->id], ['user_id', $user->id]])->first()->received_certificate_at);
     $response->assertSee(__('Congratulations! You have passed the quiz.'));
 });
+
+test('when users pass the quiz in first attempt', function () {
+    $user = User::factory()->create();
+    $course = Course::factory()->create();
+    $user->courses()->attach(
+        $course->id, ['started_at' => now(), 'finished_at' => now()]
+    );
+    $quiz = Quiz::factory()->for($course)->create();
+    $firstQuestion = Question::factory()->for($quiz)->create();
+    $firstQuestionCorrectChoice = Choice::factory()->for($firstQuestion)->create(['is_answer' => true]);
+
+    $inputData = [
+        'question_'.$firstQuestion->id => [$firstQuestionCorrectChoice->value],
+    ];
+    $response = $this->actingAs($user)
+        ->from(localized_route('quizzes.show', $quiz))
+        ->post(localized_route('quizzes.show-result', $quiz), $inputData);
+
+    $response->assertSessionHasNoErrors();
+
+    $this->assertDatabaseHas('quiz_user', [
+        'user_id' => $user->id,
+        'quiz_id' => $quiz->id,
+        'attempts' => 1,
+        'score' => 1,
+    ]);
+});
+
+test('when users fail the quiz multiple times', function () {
+    $user = User::factory()->create();
+    $course = Course::factory()->create();
+    $user->courses()->attach(
+        $course->id, ['started_at' => now(), 'finished_at' => now()]
+    );
+    $quiz = Quiz::factory()->for($course)->create();
+    $firstQuestion = Question::factory()->for($quiz)->create();
+    $firstQuestionWrongChoice = Choice::factory()->for($firstQuestion)->create();
+
+    $inputData = [
+        'question_'.$firstQuestion->id => [$firstQuestionWrongChoice->value],
+    ];
+    $response = $this->actingAs($user)
+        ->from(localized_route('quizzes.show', $quiz))
+        ->post(localized_route('quizzes.show-result', $quiz), $inputData);
+
+    $response->assertSessionHasNoErrors();
+
+    $this->assertDatabaseHas('quiz_user', [
+        'user_id' => $user->id,
+        'quiz_id' => $quiz->id,
+        'attempts' => 1,
+        'score' => 0,
+    ]);
+
+    $response = $this->actingAs($user->refresh())
+        ->from(localized_route('quizzes.show', $quiz))
+        ->post(localized_route('quizzes.show-result', $quiz), $inputData);
+
+    $response->assertSessionHasNoErrors();
+
+    $this->assertDatabaseHas('quiz_user', [
+        'user_id' => $user->id,
+        'quiz_id' => $quiz->id,
+        'attempts' => 2,
+        'score' => 0,
+    ]);
+});
