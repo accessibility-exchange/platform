35403e6e5ed1b93fffa7f8b6e37c5e32cec01d0a
Ned Zimmerman <ned@bight.dev>
2022-12-06 22:59:35 -0400

    fix: remove more unused code

--- app/Models/User.php
+++ app/Models/User.php
@@ -222,11 +222,6 @@ class User extends Authenticatable implements CipherSweetEncrypted, FilamentUser
         return $this->hasOne(Individual::class);
     }
 
-    public function resources(): HasMany
-    {
-        return $this->hasMany(Resource::class);
-    }
-
     public function resourceCollections(): HasMany
     {
         return $this->hasMany(ResourceCollection::class);
