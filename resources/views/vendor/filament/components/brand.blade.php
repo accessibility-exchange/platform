4a02f93004d171e19aff5a7cea46e69dd172d79c
Ned Zimmerman <ned@bight.dev>
2022-12-13 14:01:42 -0400

    fix: resolve merge conflict

--- resources/views/vendor/filament/components/brand.blade.php
+++ resources/views/vendor/filament/components/brand.blade.php
@@ -1,10 +1,7 @@
-<<<<<<< HEAD @if (locale() == 'en' || locale() == 'asl')
+@if (locale() == 'en' || locale() == 'asl')
     @svg('tae-logo-en', ['class' => 'logo h-10'])
     @svg('tae-logo-mono-en', ['class' => 'logo logo--themeable h-10'])
 @elseif(locale() == 'fr' || locale() == 'lsq')
     @svg('tae-logo-fr', ['class' => 'logo h-10'])
     @svg('tae-logo-mono-fr', ['class' => 'logo logo--themeable h-10'])
-    @endif
-    =======
-    @svg('tae-logo-' . (in_array(locale(), ['fr', 'lsq']) ? 'fr' : 'en'), ['class' => 'logo logo--themeable h-10'])
-    >>>>>>> dev
+@endif
