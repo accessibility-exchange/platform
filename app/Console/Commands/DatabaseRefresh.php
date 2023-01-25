e2f561fc0cbf734f7348c82b7ef7ed76751c7a45
Marvin Roman <marvin@colab.coop>
2023-01-25 15:35:30 -0800

    feat(command): added parameter to command and seeder updates
    added ability to backup seeders first. added truncating and seeding of filament tables.

--- app/Console/Commands/DatabaseRefresh.php
+++ app/Console/Commands/DatabaseRefresh.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseRefresh extends Command
 {
@@ -11,7 +12,8 @@ class DatabaseRefresh extends Command
      *
      * @var string
      */
-    protected $signature = 'db:refresh';
+    protected $signature = 'db:refresh
+                            {--backup : Whether to backup first}?';
 
     /**
      * The console command description.
@@ -27,10 +29,35 @@ class DatabaseRefresh extends Command
      */
     public function handle()
     {
+        $options = $this->options();
+
         // Don't run command in production or testing environment
         if (in_array(config('app.env'), ['testing', 'production']) !== true) {
+
+            // whether to backup seeds first
+            if ($options['backup']) {
+                $this->call('db:seed:backup', ['--all' => true]);
+            }
+
             $this->call('migrate:fresh', ['--force' => true]);
             $this->call('db:seed', ['--class' => 'DevSeeder', '--force' => true]);
+
+            print("Truncating filament tables:\r\n");
+
+            $truncate_tables = array("identities", "interpretations", "resource_collections", "resources", "topics");
+            foreach($truncate_tables as $table) {
+                printf("Truncating values in table %s.\r\n", $table);
+                DB::table($table)->delete();
+            }
+
+            print("\r\n");
+            print("Running filament seeders:\r\n");
+
+            $seeder_classes = array("IdentitySeeder", "InterpretationSeeder", "ResourceCollectionSeeder", "ResourceSeeder", "TopicSeeder");
+            foreach($seeder_classes as $seeder_class) {
+                printf("Running seeder %s.", $seeder_class);
+                $this->call('db:seed', ['--class' => $seeder_class]);
+            }
         }
 
         return 0;
