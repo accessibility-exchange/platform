81972e5723406cfa1044ceab0a1a86c4a2960e1b
Justin Obara <obara.justin@gmail.com>
2023-02-06 13:44:17 -0500

    fix: move the interpretation video below heading

--- resources/views/about/roles/community-organizations-consultation-participants.blade.php
+++ resources/views/about/roles/community-organizations-consultation-participants.blade.php
@@ -71,10 +71,7 @@
         </x-section> --}}
 
         <x-section class="accent--color text-center">
-            <p class="h3">
-                {{ __('Have more questions?') }}<br />
-                {{ __('Call our support line at :number', ['number' => phone(settings()->get('phone', '+1-888-867-0053'), 'CA')->formatForCountry('CA')]) }}
-            </p>
+            @include('partials.have-more-questions')
         </x-section>
 
         @guest
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,20 +3,20 @@
     <x-slot name="header">
         <div class="center center:wide stack stack:lg">
             <h1 itemprop="name">{{ __('The Accessibility Exchange') }}</h1>
+            <x-interpretation name="{{ __('The Accessibility Exchange', [], 'en') }}"
+                @guest namespace="welcome-guest" @endguest />
             <p class="h4">
                 {{ __('Connecting the disability and Deaf communities and their supporters with ') }}<br />{{ __('organizations and businesses to work on accessibility projects together.') }}
             </p>
             @guest
                 <p><a class="cta" href="{{ localized_route('register') }}"> {{ __('Sign up') }}</a></p>
-                <x-interpretation name="{{ __('The Accessibility Exchange', [], 'en') }}" namespace="welcome-guest" />
-            @else
-                <x-interpretation name="{{ __('The Accessibility Exchange', [], 'en') }}" />
             @endguest
         </div>
     </x-slot>
 
     <section class="stack" aria-labelledby="what">
         <h2 class="text-center" id="what">{{ __('What is the Accessibility Exchange?') }}</h2>
+        <x-interpretation name="{{ __('What is the Accessibility Exchange?', [], 'en') }}" />
         <div class="grid">
             <div class="box border--lavender">
                 <h3>{{ __('Connects the disability and Deaf communities with regulated organizations') }}
@@ -30,7 +30,6 @@
                 </p>
             </div>
         </div>
-        <x-interpretation name="{{ __('What is the Accessibility Exchange?', [], 'en') }}" />
         <div class="frame">
             <div class="stack w-full" x-data="vimeoPlayer({
                 url: @if (locale() === 'en') 'https://vimeo.com/789855323'
@@ -53,35 +52,34 @@
 
     <section class="stack stack:lg" aria-labelledby="how">
         <h2 class="text-center" id="how">{{ __('How does this work?') }}</h2>
-
+        <x-interpretation name="{{ __('How does this work?', [], 'en') }}" />
         <p class="text-center">{{ __('This site is for three kinds of users. Select an option below to learn more.') }}
         </p>
-        <x-interpretation name="{{ __('How does this work?', [], 'en') }}" />
         <div class="grid">
             <div class="flex h-full flex-col">
                 <h3><a href="{{ localized_route('about.for-individuals') }}">{{ __('For Individuals') }}</a>
                 </h3>
+                <x-interpretation class="interpretation--start" name="{{ __('For Individuals', [], 'en') }}" />
                 <p>{{ __('This is for individuals with disabilities or Deaf people and their supporters, and those wishing to offer accessibility consulting and community connection services.') }}
                 </p>
-                <x-interpretation class="interpretation--start" name="{{ __('For Individuals', [], 'en') }}" />
             </div>
             <div class="flex h-full flex-col">
                 <h3><a
                         href="{{ localized_route('about.for-regulated-organizations') }}">{{ __('For Federally Regulated Organizations') }}</a>
                 </h3>
-                <p>{{ __('Such as, federal departments, agencies, and crown corporations, other public sector bodies and businesses.') }}
-                </p>
                 <x-interpretation class="interpretation--start"
                     name="{{ __('For Federally Regulated Organizations', [], 'en') }}" />
+                <p>{{ __('Such as, federal departments, agencies, and crown corporations, other public sector bodies and businesses.') }}
+                </p>
             </div>
             <div class="flex h-full flex-col">
                 <h3><a
                         href="{{ localized_route('about.for-community-organizations') }}">{{ __('For Community Organizations') }}</a>
                 </h3>
-                <p>{{ __('This includes disability and Deaf representative organizations, support organizations, and other civil society organizations (not only disability focused).') }}
-                </p>
                 <x-interpretation class="interpretation--start"
                     name="{{ __('For Community Organizations', [], 'en') }}" />
+                <p>{{ __('This includes disability and Deaf representative organizations, support organizations, and other civil society organizations (not only disability focused).') }}
+                </p>
             </div>
         </div>
     </section>
@@ -90,10 +88,10 @@
         <div class="center center:wide stack stack:xl">
             <div class="stack">
                 <h2 id="disability">{{ __('What do we mean when we say “disability”?') }}</h2>
+                <x-interpretation name="{{ __('What do we mean when we say “disability”?', [], 'en') }}" />
                 <p class="h4">
                     {{ __('Disability is not in the person. It results when a person’s long-term physical, mental health, developmental, or sensory characteristics differ from society’s norms. When buildings, services, and workplaces are designed for the norm, they often present barriers to a person’s full and equal participation in society. That’s what we mean by disability. ') }}
                 </p>
-                <x-interpretation name="{{ __('What do we mean when we say “disability”?', [], 'en') }}" />
                 {{-- TODO: add link to glossary definition --}}
                 {{-- <p><a class="font-medium" href="">{{ __('Learn more about disability') }}</a></p> --}}
             </div>
@@ -101,18 +99,18 @@
     </section>
     <section class="stack text-center" aria-labelledby="partnership">
         <h2 id="partnership">{{ __('Developed in partnership') }}</h2>
+        <x-interpretation name="{{ __('Developed in partnership', [], 'en') }}" />
         <p class="mx-auto max-w-prose">
             {{ __('This website was made in partnership with members and organizations from the disability and Deaf communities, supporters, and members from Federally Regulated Organizations.') }}
         </p>
-        <x-interpretation name="{{ __('Developed in partnership', [], 'en') }}" />
     </section>
 
     @guest
         <section class="full accent" aria-labelledby="join">
             <div class="center center:wide stack stack:xl text-center">
                 <h2 id="join">{{ __('Join our accessibility community') }}</h2>
-                <p><a class="cta" href="{{ localized_route('register') }}"> {{ __('Sign up') }}</a></p>
                 <x-interpretation name="{{ __('Join our accessibility community', [], 'en') }}" namespace="join" />
+                <p><a class="cta" href="{{ localized_route('register') }}"> {{ __('Sign up') }}</a></p>
             </div>
         </section>
     @endguest
