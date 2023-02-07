e70964df1be28b9b7a53ed9015d1c8fe5e37a5ed
Daniel Cho <chosww@gmail.com>
2023-02-07 12:49:35 -0500

    fix: update based on PR feedback

--- resources/views/individuals/partials/progress.blade.php
+++ resources/views/individuals/partials/progress.blade.php
@@ -42,7 +42,7 @@
                     {!! Str::markdown(
                         __(
                             'You must attend an [orientation session](:url) and fill in all the required information before you can publish your page.',
-                            ['url' => orientation_link('individual')],
+                            ['url' => orientation_link(Auth::user()->context)],
                         ),
                     ) !!}
                 </p>
