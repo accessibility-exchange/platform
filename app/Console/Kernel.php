cad8f30cf54e3470849d36b06c794c76309dac88
Justin Obara <obara.justin@gmail.com>
2023-03-15 08:05:39 -0400

    fix: staging refreshed nightly

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('db:refresh --backup') // use custom command to make sure that the commands are chained
+        $schedule->command('db:seed:backup --all') // use custom command to make sure that the commands are chained
             ->daily() // Run daily at midnight
             ->environments(['staging', 'dev', 'local']) // only run for APP_ENV tagged staging, dev, or local
             ->onOneServer(); // run only on a single server at once
