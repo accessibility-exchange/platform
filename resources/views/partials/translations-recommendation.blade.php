985213d06a1ac8ceaa5ad7a03693d546f6ed9c56
Justin Obara <jobara@users.noreply.github.com>
2023-03-23 11:50:15 -0400

    fix: dismiss doesn’t work on the translation page (resolves #1425) (#1599)
    fix: dismiss doesn’t work on the translation page

---
+++ resources/views/partials/translations-recommendation.blade.php
@@ -0,0 +1,4 @@
+<x-hearth-alert :title="__('Recommendation')" x-show="true" :dismissable="false">
+    <p>{{ __('Although it is not compulsory, we highly recommend that you include English, French, American Sign Language (ASL), and Langue des signes québécoise (LSQ) translations of your content.') }}
+    </p>
+</x-hearth-alert>
