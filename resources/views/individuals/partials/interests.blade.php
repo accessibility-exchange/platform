fa4a2fabcb5c3e74b05bf3ead92fd7b33e993c96
Justin Obara <jobara@users.noreply.github.com>
2023-04-26 12:55:50 -0400

    fix: copy issues (#1677)
    * fix: update headings for public page interests
    
    * style: run php formatting
    
    * fix: location has comma even if no city specified
    
    * fix: org page shows keys for consulting services

--- resources/views/individuals/partials/interests.blade.php
+++ resources/views/individuals/partials/interests.blade.php
@@ -1,5 +1,5 @@
 @if (!$individual->sectorsOfInterest->isEmpty())
-    <h3>{{ __('Sectors of interest') }}</h3>
+    <h3>{{ __('Types of regulated organizations') }}</h3>
     <ul class="tags" role="list">
         @foreach ($individual->sectorsOfInterest as $sector)
             <li class="tag">{{ $sector->name }}</li>
@@ -7,7 +7,7 @@
     </ul>
 @endif
 @if (!$individual->impactsOfInterest->isEmpty())
-    <h3>{{ __('Areas of interest') }}</h3>
+    <h3>{{ __('Areas of accessibility') }}</h3>
     <ul class="tags" role="list">
         @foreach ($individual->impactsOfInterest as $impact)
             <li class="tag">{{ $impact->name }}</li>
