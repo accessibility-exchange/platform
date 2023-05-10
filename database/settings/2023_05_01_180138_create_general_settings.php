fb5a1ff3a016dca427dbf8f83263132d83037550
Justin Obara <obara.justin@gmail.com>
2023-05-10 13:07:39 -0400

    fix: update mailing address

--- database/settings/2023_05_01_180138_create_general_settings.php
+++ database/settings/2023_05_01_180138_create_general_settings.php
@@ -7,11 +7,11 @@ return new class extends SettingsMigration
     public function up(): void
     {
         $this->migrator->add('general.email', 'support@accessibilityexchange.ca');
-        $this->migrator->add('general.phone', '1 (888) 867-0053');
+        $this->migrator->add('general.phone', '+1-888-867-0053');
         $this->migrator->add('general.address',
             'The Accessibility Exchange ℅ IRIS
-1 University Avenue, 3rd Floor
-Toronto, ON M5J 2P1');
+20-850 King Street West
+Oshawa, ON, L1J 8N5');
         $this->migrator->add('general.facebook', 'https://facebook.com/AccessXchange');
         $this->migrator->add('general.linkedin', 'https://linkedin.com/company/the-accessibility-exchange/');
         $this->migrator->add('general.twitter', 'https://twitter.com/AccessXchange');
