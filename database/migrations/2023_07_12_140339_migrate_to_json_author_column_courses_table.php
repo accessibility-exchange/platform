d640892dff1e5fe7bad5b70a1e7421386d8881b7
Justin Obara <obara.justin@gmail.com>
2023-07-20 11:23:11 -0400

    fix: course author invalid json migration error

--- database/migrations/2023_07_12_140339_migrate_to_json_author_column_courses_table.php
+++ database/migrations/2023_07_12_140339_migrate_to_json_author_column_courses_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -13,6 +14,15 @@ return new class extends Migration
      */
     public function up()
     {
+        $courses = DB::table('courses')->get();
+
+        $courses->each(function ($course) {
+            $author = '{"en": "'.($course->author ?? '').'"}';
+            DB::table('courses')
+                ->where('id', $course->id)
+                ->update(['author' => $author]);
+        });
+
         Schema::table('courses', function (Blueprint $table) {
             $table->json('author')->change();
         });
@@ -27,6 +37,16 @@ return new class extends Migration
     {
         Schema::table('courses', function (Blueprint $table) {
             $table->string('author')->change();
+        });
+
+        $courses = DB::table('courses')->get();
+
+        $courses->each(function ($course) {
+            $author = json_decode($course->author, false);
+            $authorName = $author->en ?? $author->fr ?? '';
+            DB::table('courses')
+                ->where('id', $course->id)
+                ->update(['author' => $authorName]);
         });
     }
 };
