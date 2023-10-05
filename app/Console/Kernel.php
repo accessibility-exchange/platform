3ebebc7dd6acae7fa69cb750456fa7c2e8baf6dd
Marvin Roman <marvin@colab.coop>
2023-10-05 12:17:03 -0700

    feat(command): :sparkles: updated crons for backing up and restoring seeds

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,11 +25,16 @@ class Kernel extends ConsoleKernel
     {
         $schedule->command('db:seed:backup --all') // use custom command to make sure that the commands are chained
             ->daily() // Run daily at midnight
-            ->environments(['staging', 'dev', 'local']) // only run for APP_ENV tagged staging, dev, or local
+            ->environments(['staging', 'dev', 'local', 'production']) // only run for APP_ENV tagged staging, dev, local, or production
             ->onOneServer(); // run only on a single server at once
 
         $schedule->command('notifications:remove:old --days=30') // remove notifications older than 30 days old and read
             ->daily() // Run daily at midnight
+            ->onOneServer(); // run only on a single server at once
+
+        $schedule->command('db:seed:backup --all --restore --from=production') // restore production backups to staging, dev & local
+            ->dailyAt('00:15') // Run daily at 1 am
+            ->environments(['staging', 'dev', 'local']) // only run for APP_ENV tagged staging, dev, or local
             ->onOneServer(); // run only on a single server at once
     }
 
