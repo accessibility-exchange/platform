8ef1991a74b8270df20156d35c29ed3a692bcb96
Justin Obara <obara.justin@gmail.com>
2023-10-10 08:09:44 -0400

    fix: prefixed routes not localized
    See: https://github.com/chinleung/laravel-multilingual-routes/issues/75

--- routes/admin.php
+++ routes/admin.php
@@ -4,13 +4,12 @@ use App\Http\Controllers\AdminController;
 use App\Http\Livewire\AdminEstimatesAndAgreements;
 use App\Http\Livewire\ManageAccounts;
 
-Route::controller(AdminController::class)->prefix('admin')
-    ->name('admin.')
+Route::controller(AdminController::class)->name('admin.')
     ->group(function () {
-        Route::multilingual('/estimates-and-agreements', [AdminEstimatesAndAgreements::class, '__invoke'])
+        Route::multilingual('/admin/estimates-and-agreements', [AdminEstimatesAndAgreements::class, '__invoke'])
             ->middleware(['auth', 'admin'])
             ->name('estimates-and-agreements');
-        Route::multilingual('/accounts', [ManageAccounts::class, '__invoke'])
+        Route::multilingual('/admin/accounts', [ManageAccounts::class, '__invoke'])
             ->middleware(['auth', 'admin'])
             ->name('manage-accounts');
     });
