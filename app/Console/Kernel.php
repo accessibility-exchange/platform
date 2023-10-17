aeb969ed1fd7c9511c738fe35bda431a5993fb76
Marvin Roman <marvin@colab.coop>
2023-10-17 12:15:19 -0700

    chore(command): explicity set timezone to PST for scheduled tasks

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,15 +26,18 @@ class Kernel extends ConsoleKernel
         $schedule->command('db:seed:backup --all') // use custom command to make sure that the commands are chained
             ->daily() // Run daily at midnight
             ->environments(['staging', 'dev', 'local']) // only run for APP_ENV tagged staging, dev, or local
+            ->timezone('America/Los_Angeles') // Run as PST timezone
             ->onOneServer(); // run only on a single server at once
 
         $schedule->command('migrate:fresh --seeder=DevSeeder') // use custom command to make sure that the commands are chained
             ->dailyAt('00:15') // Run daily at 12:15 am
             ->environments(['dev']) // only run for APP_ENV tagged dev
+            ->timezone('America/Los_Angeles') // Run as PST timezone
             ->onOneServer(); // run only on a single server at once
 
         $schedule->command('notifications:remove:old --days=30') // remove notifications older than 30 days old and read
             ->daily() // Run daily at midnight
+            ->timezone('America/Los_Angeles') // Run as PST timezone
             ->onOneServer(); // run only on a single server at once
     }
 
