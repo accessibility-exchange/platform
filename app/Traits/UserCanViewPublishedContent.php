39d28db0edf6561f67ab5bc4882a73d5367f1586
Daniel Cho <chosww@gmail.com>
2023-10-18 11:26:01 -0400

    fix: update logic for who can view published content

--- app/Traits/UserCanViewPublishedContent.php
+++ app/Traits/UserCanViewPublishedContent.php
@@ -9,13 +9,18 @@ trait UserCanViewPublishedContent
 {
     public function canViewPublishedContent(User $user): bool
     {
+        if ($user->isAdministrator()) {
+            return true;
+        }
+
+        $isOriented = $user->organization?->checkStatus('approved') || $user->regulatedOrganization?->checkStatus('approved') || $user->oriented_at != null;
+
         return
             $user->hasVerifiedEmail() &&
-            $user->oriented_at != null &&
+            $isOriented &&
             in_array(
                 $user->context,
                 [
-                    UserContext::Administrator->value,
                     UserContext::Individual->value,
                     UserContext::Organization->value,
                     UserContext::RegulatedOrganization->value,
