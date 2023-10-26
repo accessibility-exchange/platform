3b95fa87ad29721b529bc35ae978a513bc1fffd8
Marvin Roman <marvin@colab.coop>
2023-10-25 19:23:42 -0700

    refactor: migrated production to dev seeder to the DevRefresh command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,24 +25,18 @@ class Kernel extends ConsoleKernel
     {
         $schedule->command('db:seed:backup --all') // use custom command to make sure that the commands are chained
             ->daily() // Run daily at midnight
-            ->environments(['staging', 'dev', 'local']) // only run for APP_ENV tagged staging, dev, or local
+            ->environments(['staging', 'dev', 'local', 'production']) // only run for APP_ENV tagged staging, dev, local, or production
             ->timezone('America/Los_Angeles') // Run as PST timezone
             ->onOneServer(); // run only on a single server at once
 
-        $schedule->command('migrate:fresh --seeder=DevSeeder') // use custom command to make sure that the commands are chained
+        $schedule->command('app:refresh-dev') // use custom hcommand to make sure that te commands are chained
             ->dailyAt('00:15') // Run daily at 12:15 am
-            ->environments(['dev']) // only run for APP_ENV tagged dev
+            ->environments(['staging', 'dev', 'local']) // only run for APP_ENV tagged staging, dev, or local
             ->timezone('America/Los_Angeles') // Run as PST timezone
             ->onOneServer(); // run only on a single server at once
 
         $schedule->command('notifications:remove:old --days=30') // remove notifications older than 30 days old and read
             ->daily() // Run daily at midnight
-            ->timezone('America/Los_Angeles') // Run as PST timezone
-            ->onOneServer(); // run only on a single server at once
-
-        $schedule->command('db:seed:backup --all --restore --from=production') // restore production backups to staging, dev & local
-            ->dailyAt('00:30') // Run daily at 12:30 am
-            ->environments(['staging', 'dev', 'local']) // only run for APP_ENV tagged staging, dev, or local
             ->timezone('America/Los_Angeles') // Run as PST timezone
             ->onOneServer(); // run only on a single server at once
     }
