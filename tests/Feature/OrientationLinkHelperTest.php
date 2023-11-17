d2a2619c25244d43ad5a771c37a80fc9a35fea3e
Daniel Cho <chosww@gmail.com>
2023-11-17 16:39:46 -0500

    fix: update OrientationLinkHelperTest with new link structure

--- tests/Feature/OrientationLinkHelperTest.php
+++ tests/Feature/OrientationLinkHelperTest.php
@@ -3,17 +3,23 @@
 use App\Enums\UserContext;
 
 test('user context individual', function () {
-    $expected = json_decode(DB::table('settings')->where('name', 'individual_orientation')->first()->payload);
+    $expected = json_decode(DB::table('settings')->where('name', 'individual_orientation')->first()->payload, true)['en'];
+    expect(orientation_link(UserContext::Individual->value))->toEqual($expected);
+});
+
+test('user context individual for Frecnh', function () {
+    App::setLocale('fr');
+    $expected = json_decode(DB::table('settings')->where('name', 'individual_orientation')->first()->payload, true)['fr'];
     expect(orientation_link(UserContext::Individual->value))->toEqual($expected);
 });
 
 test('user context organization', function () {
-    $expected = json_decode(DB::table('settings')->where('name', 'org_orientation')->first()->payload);
+    $expected = json_decode(DB::table('settings')->where('name', 'org_orientation')->first()->payload, true)['en'];
     expect(orientation_link(UserContext::Organization->value))->toEqual($expected);
 });
 
 test('user context regulated-organization', function () {
-    $expected = json_decode(DB::table('settings')->where('name', 'fro_orientation')->first()->payload);
+    $expected = json_decode(DB::table('settings')->where('name', 'fro_orientation')->first()->payload, true)['en'];
     expect(orientation_link(UserContext::RegulatedOrganization->value))->toEqual($expected);
 });
 
