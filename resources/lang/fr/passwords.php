33756d2b0656eef6e29d2de09e3fa80c9757f5dd
The Accessibility Exchange <120509736+theaccessibilityexchange@users.noreply.github.com>
2023-11-21 10:10:00 -0500

    chore(localization): translate passwords.php into French, Canada

--- resources/lang/fr/passwords.php
+++ resources/lang/fr/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'reset' => 'Vous avez réinitialisé avec succès votre mot de passe pour le site Connecteur pour l\'accessibilité.',
-    'sent' => 'Nous vous avons envoyé par courriel un lien de réinitialisation du mot de passe pour le site Connecteur pour l\'accessibilité. Veuillez vérifier votre courriel.',
+    'reset' => 'Vous avez réinitialisé avec succès votre mot de passe pour le site Connecteur pour l’accessibilité.',
+    'sent' => 'Nous vous avons envoyé par courriel un lien de réinitialisation du mot de passe pour le site Connecteur pour l’accessibilité. Veuillez vérifier votre courriel.',
     'throttled' => 'Veuillez patienter avant de réessayer.',
-    'token' => 'Ce jeton de réinitialisation du mot de passe n\'est pas valide.',
-    'user' => 'We can’t find an account with that email address.',
+    'token' => 'Ce jeton de réinitialisation du mot de passe n’est pas valide.',
+    'user' => 'Nous ne pouvons pas trouver de compte associé à cette adresse courriel.',
 
 ];
