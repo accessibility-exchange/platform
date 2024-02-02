71af6c5ecec1f68687cb815d748df953b83ac342
The Accessibility Exchange <120509736+theaccessibilityexchange@users.noreply.github.com>
2024-02-02 07:23:54 -0500

    chore(localization): translate passwords.php into French, Canada

--- resources/lang/vendor/hearth/fr/passwords.php
+++ resources/lang/vendor/hearth/fr/passwords.php
@@ -17,6 +17,6 @@ return [
     'sent' => 'Nous avons envoyé votre lien de réinitialisation de mot de passe!',
     'throttled' => 'Veuillez patienter avant de réessayer.',
     'token' => 'Ce jeton de réinitialisation de mot de passe est invalide.',
-    'user' => 'Nous ne pouvons pas trouver un utilisateur avec cette adresse e-mail.',
+    'user' => "Nous ne pouvons pas trouver un utilisateur avec cette adresse e-mail.",
 
 ];
