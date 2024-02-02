b2db3b5104e7d5428229332cd763e7b953324ae5
Justin Obara <obara.justin@gmail.com>
2024-02-02 06:58:40 -0500

    fix: add hearth translations to crowdin

---
+++ resources/lang/vendor/hearth/en/auth.php
@@ -0,0 +1,48 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'sign_in' => 'Sign in',
+    'sign_out' => 'Sign out',
+    'create_account' => 'Create an account',
+    'label_password' => 'Password',
+    'label_password_confirmation' => 'Confirm password',
+    'existing_account_prompt' => 'Do you already have an account?',
+    'create_your_account' => 'Create your account',
+    'label_current_password' => 'Current password',
+    'change_password' => 'Change password',
+    'action_cancel' => 'Cancel',
+    'action_confirm' => 'Confirm',
+    'password_change_succeeded' => 'Your password has been changed.',
+    'label_remember_me' => 'Remember me',
+    'error_intro' => 'Sorry, something went wrong.',
+    'wrong_password' => 'The provided password is incorrect.',
+    'confirm_intro' => 'For your security, please confirm your password before continuing.',
+    'forget_prompt' => 'Forgot your password?',
+    'forgot_intro' => 'Forgot your password? Enter your email address and we will email you a password reset link that will let you choose a new one.',
+    'forgot_submit' => 'Send password reset link',
+    'reset_submit' => 'Reset password',
+    'verification_required' => 'Email verification required',
+    'verification_intro' => 'Please verify your email address by clicking on the link we emailed to you. If you didn’t receive the email, we will gladly send you another.',
+    'verification_sent' => 'A new verification link has been sent to the email address you provided.',
+    'resend_verification_email' => 'Resend verification email',
+    'verification_succeeded' => 'Your email address has been verified.',
+    'two_factor_auth_code_intro' => 'Please enter the code provided by your authenticator application to complete the sign in process.',
+    'label_two_factor_auth_code' => 'Authentication code',
+    'two_factor_auth_action_use_recovery_code' => 'Use a recovery code',
+    'two_factor_auth_recovery_code_intro' => 'Please enter one of your saved recovery codes to complete the sign in process.',
+    'label_two_factor_auth_recovery_code' => 'Recovery code',
+    'two_factor_auth_action_use_code' => 'Use an authentication code',
+    'invalid_two_factor_auth_code' => 'The provided two-factor authentication code was not valid.',
+];
---
+++ resources/lang/vendor/hearth/en/user.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    'settings' => 'Settings',
+    'account' => 'Account',
+    'label_name' => 'Full name',
+    'label_locale' => 'Preferred language',
+    'two_factor_auth' => 'Two-factor authentication',
+    'two_factor_auth_intro' => 'Add additional security to your account using two-factor authentication.',
+    'two_factor_auth_not_enabled' => 'You have not enabled two-factor authentication.',
+    'two_factor_auth_enabled' => 'You have enabled two-factor authentication.',
+    'two_factor_auth_disabled' => 'You have disabled two-factor authentication.',
+    'two_factor_auth_qr_code' => 'Two factor authentication is now enabled. Scan the following QR code using your phone\'s authenticator application.',
+    'two_factor_auth_recovery_codes' => 'Store these recovery codes in a secure password manager. They can be used to recover access to your account if your two factor authentication device is lost.',
+    'two_factor_auth_recovery_codes_regenerated' => 'New recovery codes have been generated.',
+    'action_enable_two_factor_auth' => 'Enable two-factor authentication',
+    'action_disable_two_factor_auth' => 'Disable two-factor authentication',
+    'action_regenerate_two_factor_auth_recovery_codes' => 'Regenerate recovery codes',
+    'delete_account' => 'Delete account',
+    'delete_account_intro' => 'Your account will be deleted and cannot be recovered. If you still want to delete your account, please enter your current password to proceed.',
+    'action_delete_account' => 'Delete my account',
+    'settings_update_succeeded' => 'Your settings have been saved.',
+    'destroy_succeeded' => 'Your account has been deleted.',
+];
