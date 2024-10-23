2a1703af5e430f0c746aee8196c08c9c522ea2d6
Ned Zimmerman <ned@bight.dev>
2024-10-23 12:07:38 -0300

    fix: resolve linting errors

--- resources/js/datePicker.js
+++ resources/js/datePicker.js
@@ -31,15 +31,15 @@ export default (initial = "") => ({
      */
     daysInMonth(m, y) {
         switch (m) {
-            case 1:
-                return (y % 4 === 0 && y % 100) || y % 400 === 0 ? 29 : 28;
-            case 8:
-            case 3:
-            case 5:
-            case 10:
-                return 30;
-            default:
-                return 31;
+        case 1:
+            return (y % 4 === 0 && y % 100) || y % 400 === 0 ? 29 : 28;
+        case 8:
+        case 3:
+        case 5:
+        case 10:
+            return 30;
+        default:
+            return 31;
         }
     },
     /**
