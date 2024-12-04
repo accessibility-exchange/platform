6f5e3345834d9b0ac2c7eae60c8b82c8e1a271df
Ned Zimmerman <ned@bight.dev>
2024-12-04 09:51:55 -0400

    fix: resolve issue with Symfony Mailer 7.2 (#2412)

--- config/mail.php
+++ config/mail.php
@@ -35,6 +35,7 @@ return [
 
     'mailers' => [
         'smtp' => [
+            'scheme' => 'smtp', // Remove once https://github.com/laravel/framework/pull/53585 appears in a Laravel release
             'transport' => 'smtp',
             'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
             'port' => env('MAIL_PORT', 587),
