cecfee376054da4c43dfa088f00f47d37c6d89da
Marvin Roman <marvin@colab.coop>
2024-12-10 23:37:58 +0800

    build: created nix shell file to automatically build and include dependencies

---
+++ shell.nix
@@ -0,0 +1,58 @@
+let
+  unstable = import (fetchTarball https://nixos.org/channels/nixos-unstable/nixexprs.tar.xz) { };
+in
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    docker
+    docker-compose
+    envsubst
+    git
+    gnused
+    kubectl
+    nodejs_22
+    openssl
+    rootlesskit
+    unstable.php84
+    unstable.php84Packages.composer
+  ];
+
+  # Ensure Docker service is running
+  shellHook = ''
+    export DOCKER_HOST=unix://$XDG_RUNTIME_DIR/docker.sock
+
+    # setup aliases
+    alias dc="docker-compose -f docker-compose.local.yml"
+    alias kcd="kubectl -n iris-accessibility-development"
+    alias kcs="kubectl -n iris-accessibility-staging"
+    alias kcp="kubectl -n iris-accessibility-production"
+
+    # setup environment file
+    if [[ ! -f ".env" ]]; then
+        export CIPHERSWEET_KEY=$(openssl rand -hex 32)
+        export DB_PASSWORD=$(openssl rand -hex 32)
+        export REDIS_PASSWORD=$(openssl rand -hex 20)
+        export APP_KEY=$(openssl rand -hex 32 | base64 -w 0)
+        export WWWUSER=$UID
+        export WWWGROUP=$GID
+        envsubst < .env.local.example > .env
+    fi
+
+    # install composer dependencies
+    if [[ ! -d "vendor" ]]; then
+        composer install
+    fi
+
+    # install node modules
+    if [[ ! -d "node_modules" ]]; then
+        npm install
+    fi
+
+    if [[ ! -d "~/.kube" ]]; then
+        mkdir -p ~/.kube
+    fi
+  '';
+
+
+}
