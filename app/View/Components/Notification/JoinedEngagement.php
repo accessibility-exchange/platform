7f0970854ff8a9fe1875b98f4e411c343adc6ae1
Justin Obara <556024+jobara@users.noreply.github.com>
2025-04-18 14:45:58 -0400

    feat: send notification for joining/leaving an engagement (resolves #2578) (#2614)
    * feat: notification for joining and leaving an engagement
    
    * feat: send notification for joining and leaving an engagement
    
    * fix: notifications not rendered
    
    * test: updating notified user in tests
    
    * test: remove only()

---
+++ app/View/Components/Notification/JoinedEngagement.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\View\Components\Notification;
+
+use App\Models\Engagement;
+use App\Models\Organization;
+use App\Models\RegulatedOrganization;
+use App\View\Components\Notification;
+use Illuminate\Contracts\View\View;
+use Illuminate\Notifications\DatabaseNotification;
+
+class JoinedEngagement extends Notification
+{
+    public Engagement $engagement;
+
+    public mixed $projectable;
+
+    public function __construct(DatabaseNotification $notification)
+    {
+        $this->engagement = Engagement::find($notification->data['engagement_id']);
+        $this->projectable = $this->engagement->project->projectable;
+        /** @var Organization|RegulatedOrganization */
+        $projectable = $this->projectable;
+        $this->title = __('Engagement joined');
+        $this->body = safe_markdown('You have signed up for [:engagement](:engagement_url) by [:projectable](:projectable_url).', [
+            'engagement' => $this->engagement->getTranslation('name', locale()),
+            'engagement_url' => localized_route('engagements.show', $this->engagement),
+            'projectable' => $projectable->getTranslation('name', locale()),
+            'projectable_url' => localized_route($projectable->getRoutePrefix().'.show', $projectable),
+        ]);
+        $this->interpretation = __('Engagement joined', [], 'en');
+
+        parent::__construct($notification);
+    }
+
+    public function render(): View
+    {
+        return view('components.notification.joined-engagement', [
+            'notification' => $this->notification,
+            'read' => ! is_null($this->notification->read_at),
+            'title' => $this->title,
+            'body' => $this->body,
+            'engagement' => $this->engagement,
+            'interpretation' => $this->interpretation,
+        ]);
+    }
+}
