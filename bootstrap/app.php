fb5934e12239cbfbf86f43061764b7b5f92d87fa
Ned Zimmerman <ned@bight.dev>
2025-05-07 09:05:00 -0300

    fix: config syntax for trustHosts

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -30,7 +30,7 @@ return Application::configure(basePath: dirname(__DIR__))
             'password_confirmation',
         ]);
 
-        $middleware->trustHosts(at: fn () => config('app.url'), subdomains: true);
+        $middleware->trustHosts(at: fn () => [config('app.url')], subdomains: true);
 
         $middleware->trustProxies(at: '*');
 
