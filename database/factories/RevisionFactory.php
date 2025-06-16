62257573e1de086ea4dd944021f4977dd831fa34
Ned Zimmerman <ned@bight.dev>
2025-06-16 13:18:02 -0300

    fix: unique dates in factory

--- database/factories/RevisionFactory.php
+++ database/factories/RevisionFactory.php
@@ -4,18 +4,19 @@ namespace Database\Factories;
 
 use App\Models\Document;
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Carbon;
+
+use function Pest\Faker\fake;
 
 class RevisionFactory extends Factory
 {
     public function definition(): array
     {
-        $now = Carbon::now()->format('Y-m-d');
+        $date = fake()->date('Y-m-d');
 
         return [
             'document_id' => Document::factory(),
-            'date' => $now,
-            'file' => ['en' => "documents/example-document-$now-en.pdf"],
+            'date' => $date,
+            'file' => ['en' => "documents/example-document-$date-en.pdf"],
         ];
     }
 }
