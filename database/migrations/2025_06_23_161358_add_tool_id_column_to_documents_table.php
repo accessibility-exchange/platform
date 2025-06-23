ae2e1d37a3d5d8d1e2e2e8340de1c557fdbca603
Ned Zimmerman <ned@bight.dev>
2025-06-23 16:41:20 -0300

    fix: don't cascade delete

--- database/migrations/2025_06_23_161358_add_tool_id_column_to_documents_table.php
+++ database/migrations/2025_06_23_161358_add_tool_id_column_to_documents_table.php
@@ -11,8 +11,7 @@ return new class extends Migration
         Schema::table('documents', function (Blueprint $table) {
             $table->foreignId('tool_id')
                 ->nullable()
-                ->constrained()
-                ->onDelete('cascade');
+                ->constrained();
         });
     }
 
