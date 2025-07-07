a150ac81c264ebe516d44308f0bdced3bb68d554
Ned Zimmerman <ned@bight.dev>
2025-07-07 10:22:04 -0300

    fix: remove user resolver

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers;
 
-use App\Enums\UserContext;
 use App\Models\Engagement;
 use App\Models\Individual;
 use App\Models\Organization;
@@ -30,9 +29,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Validation\Rules\Password;
-use Laravel\Pulse\Facades\Pulse;
 use Makeable\EloquentStatus\StatusManager;
-use Ramsey\Uuid\Uuid;
 use Spatie\LaravelIgnition\Facades\Flare;
 use Spatie\Translatable\Facades\Translatable;
 
@@ -77,11 +74,6 @@ class AppServiceProvider extends ServiceProvider
         Flare::determineVersionUsing(function () {
             return InstalledVersions::getRootPackage()['pretty_version'];
         });
-
-        Pulse::user(fn ($user) => [
-            'name' => Uuid::uuid5(config('app.id_namespace'), $user->id),
-            'extra' => UserContext::labels()[$user->context],
-        ]);
 
         StatusManager::bind(Engagement::class, EngagementStatus::class);
         StatusManager::bind(Individual::class, IndividualStatus::class);
--- config/app.php
+++ config/app.php
@@ -6,5 +6,4 @@ return [
         'blocking' => false,
         'lived-experience-notifications' => false,
     ],
-    'id_namespace' => '43e73dc6-5359-11f0-b104-6a0e189d6221',
 ];
