e09f73ce45235a01657edc14e56325ca79fe18c2
Justin Obara <obara.justin@gmail.com>
2025-07-17 08:49:09 -0400

    fix: broken test

--- tests/Feature/EngagementTest.php
+++ tests/Feature/EngagementTest.php
@@ -278,15 +278,15 @@ test('notifications for Individual users are sent when new open-call engagements
 });
 
 test('view notifications for Individual users about new open-call engagements', function () {
-    $userWithNotifications = User::factory()->create([
+    $userWithNotifications = User::factory()->hasIndividual()->create([
         'context' => UserContext::Individual->value,
         'notification_settings' => ['engagements' => '1'],
     ]);
-    $userWithoutNotifications = User::factory()->create([
+    $userWithoutNotifications = User::factory()->hasIndividual()->create([
         'context' => UserContext::Individual->value,
         'notification_settings' => ['engagements' => '0'],
     ]);
-    $suspendedUser = User::factory()->create([
+    $suspendedUser = User::factory()->hasIndividual()->create([
         'context' => UserContext::Individual->value,
         'notification_settings' => ['engagements' => '1'],
         'suspended_at' => now(),
@@ -330,7 +330,7 @@ test('view notifications for Individual users about new open-call engagements', 
     actingAs($suspendedUser)->get(localized_route('dashboard.notifications'))
         ->assertOk()
         ->assertDontSee(__('New engagement added'));
-});
+})->only();
 
 test('notifications are not sent for Individual users when an non-open-call engagement is published', function () {
     Notification::fake();
