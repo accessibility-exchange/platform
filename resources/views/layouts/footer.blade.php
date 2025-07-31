4cfeb10f3619586a988f9fb5babc1ab8f41da16a
Justin Obara <556024+jobara@users.noreply.github.com>
2025-07-31 10:02:21 -0400

    fix: vrs URL should be included in the website settings (resolves #2845) (#2846)
    fix: vrs URL should be included in the website settings

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -33,10 +33,7 @@
                                 href="mailto:{{ settings_localized('email', locale()) }}">{{ settings_localized('email', locale()) }}</a>
                         </p>
                         <h3>{{ safe_inlineMarkdown('Call or :!vrs', [
-                            'vrs' =>
-                                '<a href="https://srvcanadavrs.ca/en/resources/resource-centre/vrs-basics/register/" rel="external">' .
-                                htmlentities(__('VRS')) .
-                                '</a>',
+                            'vrs' => '<a href="' . settings_localized('vrs', locale()) . '" rel="external">' . htmlentities(__('VRS')) . '</a>'
                         ]) }}
                         </h3>
                         <p>{{ phone(settings_localized('phone', locale(), '+1-888-867-0053'), 'CA')->formatForCountry('CA') }}
