67e7e95059534d5e92351a5b1e568773da9a09ad
Ned Zimmerman <ned@bight.dev>
2025-08-26 16:36:29 -0300

    fix: remove unneeded path style endpoint config

--- config/filesystems.php
+++ config/filesystems.php
@@ -75,7 +75,6 @@ return [
             'bucket' => env('DOCUMENTS_AWS_BUCKET'),
             'url' => env('DOCUMENTS_AWS_URL'),
             'endpoint' => env('DOCUMENTS_AWS_ENDPOINT'),
-            'use_path_style_endpoint' => env('DOCUMENTS_AWS_USE_PATH_STYLE_ENDPOINT'),
         ],
 
     ],
