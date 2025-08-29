de2c03b22820657dde54cbe3d478328d1600a9bb
Ned Zimmerman <ned@bight.dev>
2025-08-29 09:14:00 -0300

    fix: use function for nav item URLs

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -42,15 +42,15 @@ class AdminPanelProvider extends PanelProvider
             ->pages([])
             ->navigationItems([
                 NavigationItem::make(__('Dashboard'))
-                    ->url(localized_route('dashboard'))
+                    ->url(fn () => localized_route('dashboard'))
                     ->icon('heroicon-m-view-columns')
                     ->sort(-3),
                 NavigationItem::make(__('Manage accounts'))
-                    ->url(localized_route('admin.manage-accounts'))
+                    ->url(fn () => localized_route('admin.manage-accounts'))
                     ->sort(-2)
                     ->group(__('Manage')),
                 NavigationItem::make(__('Estimates and agreements'))
-                    ->url(localized_route('admin.estimates-and-agreements'))
+                    ->url(fn () => localized_route('admin.estimates-and-agreements'))
                     ->sort(-1)
                     ->group(__('Manage')),
             ])
